7f4b07fb8a9878aeeb0146545d0cb92722f7c62f
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:15:37 +0800

    fix: cast math.js factory map in instance provider

--- src/infrastructure/mathjs/DefaultMathJsInstanceProvider.ts
+++ src/infrastructure/mathjs/DefaultMathJsInstanceProvider.ts
@@ -1,11 +1,16 @@
-import { create, all, type MathJsInstance } from "mathjs";
-import { MathJsInstanceProvider } from "./MathJsInstanceProvider";
+import {
+  create,
+  all,
+  type MathJsInstance,
+  type FactoryFunctionMap,
+} from "mathjs";
+import type { MathJsInstanceProvider } from "./MathJsInstanceProvider";
 
 export class DefaultMathJsInstanceProvider implements MathJsInstanceProvider {
   private readonly instance: MathJsInstance;
 
   public constructor() {
-    this.instance = create(all, {});
+    this.instance = create(all as FactoryFunctionMap, {});
   }
 
   public getInstance(): MathJsInstance {
