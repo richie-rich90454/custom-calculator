362092eaefb13bfc924304736631e9bc69adedad
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:00 +0800

    style: format src/domain/services/ScientificFunctionCatalogService.ts with oxfmt

--- src/domain/services/ScientificFunctionCatalogService.ts
+++ src/domain/services/ScientificFunctionCatalogService.ts
@@ -1,8 +1,8 @@
 import { ScientificFunctionDefinition } from "./ScientificFunctionDefinition";
 
 export interface ScientificFunctionCatalogService {
-  getAllFunctions(): readonly ScientificFunctionDefinition[];
-  getFunction(functionName: string): ScientificFunctionDefinition | null;
-  hasFunction(functionName: string): boolean;
-  getFunctionNames(): readonly string[];
+    getAllFunctions(): readonly ScientificFunctionDefinition[];
+    getFunction(functionName: string): ScientificFunctionDefinition | null;
+    hasFunction(functionName: string): boolean;
+    getFunctionNames(): readonly string[];
 }
