18817d011a88bc564f7d2376be46e9d7951fce17
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:10 +0800

    style: format src/infrastructure/persistence/InMemoryVariablesRepository.ts with oxfmt

--- src/infrastructure/persistence/InMemoryVariablesRepository.ts
+++ src/infrastructure/persistence/InMemoryVariablesRepository.ts
@@ -2,35 +2,35 @@ import { VariableAssignment } from "../../domain/model/VariableAssignment";
 import type { VariablesRepository } from "../../domain/repositories/VariablesRepository";
 
 export class InMemoryVariablesRepository implements VariablesRepository {
-  private readonly variables: VariableAssignment[] = [];
+    private readonly variables: VariableAssignment[] = [];
 
-  public async loadVariables(): Promise<readonly VariableAssignment[]> {
-    return [...this.variables];
-  }
-
-  public async saveVariable(variable: VariableAssignment): Promise<void> {
-    const existingIndex = this.variables.findIndex(
-      (candidate) => candidate.name === variable.name
-    );
-
-    if (existingIndex >= 0) {
-      this.variables.splice(existingIndex, 1, variable);
-    } else {
-      this.variables.push(variable);
+    public async loadVariables(): Promise<readonly VariableAssignment[]> {
+        return [...this.variables];
     }
-  }
 
-  public async deleteVariable(variableName: string): Promise<void> {
-    const existingIndex = this.variables.findIndex(
-      (candidate) => candidate.name === variableName
-    );
+    public async saveVariable(variable: VariableAssignment): Promise<void> {
+        const existingIndex = this.variables.findIndex(
+            (candidate) => candidate.name === variable.name,
+        );
 
-    if (existingIndex >= 0) {
-      this.variables.splice(existingIndex, 1);
+        if (existingIndex >= 0) {
+            this.variables.splice(existingIndex, 1, variable);
+        } else {
+            this.variables.push(variable);
+        }
     }
-  }
 
-  public async clearVariables(): Promise<void> {
-    this.variables.length = 0;
-  }
+    public async deleteVariable(variableName: string): Promise<void> {
+        const existingIndex = this.variables.findIndex(
+            (candidate) => candidate.name === variableName,
+        );
+
+        if (existingIndex >= 0) {
+            this.variables.splice(existingIndex, 1);
+        }
+    }
+
+    public async clearVariables(): Promise<void> {
+        this.variables.length = 0;
+    }
 }
