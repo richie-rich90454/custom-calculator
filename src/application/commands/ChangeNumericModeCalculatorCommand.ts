f14c8f9f0ac3d45f1e8d5292f7308f54e2e619ae
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:24:23 +0800

    feat: add change numeric mode calculator command

---
+++ src/application/commands/ChangeNumericModeCalculatorCommand.ts
@@ -0,0 +1,30 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import { NumericMode } from "../../domain/model/NumericMode";
+import type { NumericModePolicyService } from "../../domain/services/NumericModePolicyService";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class ChangeNumericModeCalculatorCommand extends AbstractCalculatorCommand {
+  public constructor(
+    private readonly numericModePolicyService: NumericModePolicyService,
+    private readonly requestedNumericMode: NumericMode
+  ) {
+    super();
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    const effectiveNumericMode =
+      this.numericModePolicyService.resolveEffectiveNumericMode(
+        this.requestedNumericMode
+      );
+
+    return currentState.copyWith({
+      numericMode: effectiveNumericMode,
+      resultText: null,
+      errorText: null,
+      lastResultValue: null,
+      lastResultText: null,
+    });
+  }
+}
