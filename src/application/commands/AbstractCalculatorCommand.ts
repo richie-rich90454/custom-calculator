af1af6ac4016c7dd7bc8761e095d3e90c722bf36
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:21:40 +0800

    feat: add abstract calculator command base

---
+++ src/application/commands/AbstractCalculatorCommand.ts
@@ -0,0 +1,8 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { CalculatorCommand } from "./CalculatorCommand";
+
+export abstract class AbstractCalculatorCommand implements CalculatorCommand {
+  public abstract execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState;
+}
