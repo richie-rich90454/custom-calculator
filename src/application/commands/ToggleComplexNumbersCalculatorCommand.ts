7d484f1bb89823b0d65db46acec6419fa7b54a96
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:24:35 +0800

    feat: add toggle complex numbers calculator command

---
+++ src/application/commands/ToggleComplexNumbersCalculatorCommand.ts
@@ -0,0 +1,16 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class ToggleComplexNumbersCalculatorCommand extends AbstractCalculatorCommand {
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    return currentState.copyWith({
+      complexNumbersEnabled: !currentState.complexNumbersEnabled,
+      resultText: null,
+      errorText: null,
+      lastResultValue: null,
+      lastResultText: null,
+    });
+  }
+}
