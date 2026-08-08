25c4b9fae8a312fc9cd316c5c1fddd0752cf37b8
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:20 +0800

    style: format src/presentation/components/CalculatorDisplayComponent.tsx with oxfmt

--- src/presentation/components/CalculatorDisplayComponent.tsx
+++ src/presentation/components/CalculatorDisplayComponent.tsx
@@ -7,20 +7,20 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/CalculatorDisplayComponent.module.css";
 
 export function CalculatorDisplayComponent() {
-  const { store } = useCalculatorApplicationContext();
-  const viewModel = useCalculatorViewModel();
+    const { store } = useCalculatorApplicationContext();
+    const viewModel = useCalculatorViewModel();
 
-  return (
-    <div className={cssClass(styles.display)}>
-      <CalculatorExpressionEditorComponent
-        expressionText={viewModel.expressionText}
-        selectionStart={viewModel.selectionStart}
-        selectionEnd={viewModel.selectionEnd}
-        errorText={viewModel.errorText}
-        actions={store.getState()}
-      />
-      <CalculatorResultLineComponent resultText={viewModel.resultText} />
-      <CalculatorErrorLineComponent errorText={viewModel.errorText} />
-    </div>
-  );
+    return (
+        <div className={cssClass(styles.display)}>
+            <CalculatorExpressionEditorComponent
+                expressionText={viewModel.expressionText}
+                selectionStart={viewModel.selectionStart}
+                selectionEnd={viewModel.selectionEnd}
+                errorText={viewModel.errorText}
+                actions={store.getState()}
+            />
+            <CalculatorResultLineComponent resultText={viewModel.resultText} />
+            <CalculatorErrorLineComponent errorText={viewModel.errorText} />
+        </div>
+    );
 }
