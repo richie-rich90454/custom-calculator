7c99bf0cc9e1b2a61c702f39bf863ed86526cd04
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 14:34:11 +0800

    feat: load Noto Sans and Noto Sans Mono font faces

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css";
+import "@fontsource/noto-sans-mono/400.css";
+import "@fontsource/noto-sans-mono/500.css";
+import "@fontsource/noto-sans-mono/600.css";
 import "./presentation/styles/themes.css";
 import "./presentation/styles/global.css";
 import { CalculatorApplicationBootstrap } from "./app/CalculatorApplicationBootstrap";
