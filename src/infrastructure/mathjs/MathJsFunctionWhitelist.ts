46f000f039df48f6417827c6d0d2a267cbdbbdf8
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:09 +0800

    style: format src/infrastructure/mathjs/MathJsFunctionWhitelist.ts with oxfmt

--- src/infrastructure/mathjs/MathJsFunctionWhitelist.ts
+++ src/infrastructure/mathjs/MathJsFunctionWhitelist.ts
@@ -1,15 +1,15 @@
 export class MathJsFunctionWhitelist {
-  private readonly allowedFunctionNames: ReadonlySet<string>;
+    private readonly allowedFunctionNames: ReadonlySet<string>;
 
-  public constructor(allowedFunctionNames: readonly string[]) {
-    this.allowedFunctionNames = new Set(allowedFunctionNames);
-  }
+    public constructor(allowedFunctionNames: readonly string[]) {
+        this.allowedFunctionNames = new Set(allowedFunctionNames);
+    }
 
-  public isFunctionAllowed(functionName: string): boolean {
-    return this.allowedFunctionNames.has(functionName);
-  }
+    public isFunctionAllowed(functionName: string): boolean {
+        return this.allowedFunctionNames.has(functionName);
+    }
 
-  public getAllowedFunctionNames(): readonly string[] {
-    return [...this.allowedFunctionNames];
-  }
+    public getAllowedFunctionNames(): readonly string[] {
+        return [...this.allowedFunctionNames];
+    }
 }
