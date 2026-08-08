d60011177ec235ade91c1a12a30df4c21888961a
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:48 +0800

    style: format src/domain/model/VariableAssignment.ts with oxfmt

--- src/domain/model/VariableAssignment.ts
+++ src/domain/model/VariableAssignment.ts
@@ -1,10 +1,10 @@
 import { NumericMode } from "./NumericMode";
 
 export class VariableAssignment {
-  public constructor(
-    public readonly name: string,
-    public readonly valueText: string,
-    public readonly numericMode: NumericMode,
-    public readonly updatedAt: string
-  ) {}
+    public constructor(
+        public readonly name: string,
+        public readonly valueText: string,
+        public readonly numericMode: NumericMode,
+        public readonly updatedAt: string,
+    ) {}
 }
