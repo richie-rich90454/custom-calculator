f28d6f9b95da7a1ffd826ec14215fedf111ad016
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:45 +0800

    style: format src/domain/model/CalculatorSettings.ts with oxfmt

--- src/domain/model/CalculatorSettings.ts
+++ src/domain/model/CalculatorSettings.ts
@@ -3,21 +3,21 @@ import { NumericMode } from "./NumericMode";
 import { ThemePreference } from "./ThemePreference";
 
 export class CalculatorSettings {
-  public constructor(
-    public readonly angleMode: AngleMode,
-    public readonly numericMode: NumericMode,
-    public readonly complexNumbersEnabled: boolean,
-    public readonly casEnabled: boolean,
-    public readonly themePreference: ThemePreference
-  ) {}
+    public constructor(
+        public readonly angleMode: AngleMode,
+        public readonly numericMode: NumericMode,
+        public readonly complexNumbersEnabled: boolean,
+        public readonly casEnabled: boolean,
+        public readonly themePreference: ThemePreference,
+    ) {}
 
-  public static createDefault(): CalculatorSettings {
-    return new CalculatorSettings(
-      AngleMode.DEG,
-      NumericMode.STANDARD,
-      false,
-      false,
-      ThemePreference.SYSTEM
-    );
-  }
+    public static createDefault(): CalculatorSettings {
+        return new CalculatorSettings(
+            AngleMode.DEG,
+            NumericMode.STANDARD,
+            false,
+            false,
+            ThemePreference.SYSTEM,
+        );
+    }
 }
