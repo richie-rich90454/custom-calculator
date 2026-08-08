87b0316574af72e83167370a29087931ad0034a6
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:48 +0800

    style: format src/presentation/services/CalculatorKeyDefinition.ts with oxfmt

--- src/presentation/services/CalculatorKeyDefinition.ts
+++ src/presentation/services/CalculatorKeyDefinition.ts
@@ -1,17 +1,17 @@
 export enum CalculatorKeyKind {
-  DIGIT = "DIGIT",
-  OPERATOR = "OPERATOR",
-  FUNCTION = "FUNCTION",
-  CONSTANT = "CONSTANT",
-  PARENTHESIS = "PARENTHESIS",
-  CLEAR = "CLEAR",
-  DELETE_BACKWARD = "DELETE_BACKWARD",
-  DELETE_FORWARD = "DELETE_FORWARD",
-  EVALUATE = "EVALUATE",
-  ANS = "ANS",
-  MEMORY_RECALL = "MEMORY_RECALL",
-  CAS_OPERATION = "CAS_OPERATION",
-  CALCULUS_OPERATION = "CALCULUS_OPERATION",
+    DIGIT = "DIGIT",
+    OPERATOR = "OPERATOR",
+    FUNCTION = "FUNCTION",
+    CONSTANT = "CONSTANT",
+    PARENTHESIS = "PARENTHESIS",
+    CLEAR = "CLEAR",
+    DELETE_BACKWARD = "DELETE_BACKWARD",
+    DELETE_FORWARD = "DELETE_FORWARD",
+    EVALUATE = "EVALUATE",
+    ANS = "ANS",
+    MEMORY_RECALL = "MEMORY_RECALL",
+    CAS_OPERATION = "CAS_OPERATION",
+    CALCULUS_OPERATION = "CALCULUS_OPERATION",
 }
 
 /**
@@ -21,9 +21,9 @@ export enum CalculatorKeyKind {
  * dispatch behavior testable and easily extensible without touching TSX.
  */
 export interface CalculatorKeyDefinition {
-  readonly id: string;
-  readonly label: string;
-  readonly ariaLabel: string;
-  readonly kind: CalculatorKeyKind;
-  readonly value: string;
+    readonly id: string;
+    readonly label: string;
+    readonly ariaLabel: string;
+    readonly kind: CalculatorKeyKind;
+    readonly value: string;
 }
