fc26c786a8029d381cc84e3afbbb2b45f450c321
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:29 +0800

    style: format src/application/commands/DeleteBackwardCalculatorCommand.ts with oxfmt

--- src/application/commands/DeleteBackwardCalculatorCommand.ts
+++ src/application/commands/DeleteBackwardCalculatorCommand.ts
@@ -3,28 +3,24 @@ import type { ExpressionEditingService } from "../../domain/services/ExpressionE
 import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
 
 export class DeleteBackwardCalculatorCommand extends AbstractCalculatorCommand {
-  public constructor(
-    private readonly expressionEditingService: ExpressionEditingService
-  ) {
-    super();
-  }
+    public constructor(private readonly expressionEditingService: ExpressionEditingService) {
+        super();
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    const edit = this.expressionEditingService.deleteBackward(
-      currentState.expressionText,
-      currentState.selectionStart,
-      currentState.selectionEnd
-    );
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        const edit = this.expressionEditingService.deleteBackward(
+            currentState.expressionText,
+            currentState.selectionStart,
+            currentState.selectionEnd,
+        );
 
-    return currentState.copyWith({
-      expressionText: edit.text,
-      cursorPosition: edit.cursorPosition,
-      selectionStart: edit.selectionStart,
-      selectionEnd: edit.selectionEnd,
-      resultText: null,
-      errorText: null,
-    });
-  }
+        return currentState.copyWith({
+            expressionText: edit.text,
+            cursorPosition: edit.cursorPosition,
+            selectionStart: edit.selectionStart,
+            selectionEnd: edit.selectionEnd,
+            resultText: null,
+            errorText: null,
+        });
+    }
 }
