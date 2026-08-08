d16dcf8fba146cae55c61f545b5dfdcc46182d32
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:22:59 +0800

    feat: add insert variable calculator command

---
+++ src/application/commands/InsertVariableCalculatorCommand.ts
@@ -0,0 +1,32 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { ExpressionEditingService } from "../../domain/services/ExpressionEditingService";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class InsertVariableCalculatorCommand extends AbstractCalculatorCommand {
+  public constructor(
+    private readonly expressionEditingService: ExpressionEditingService,
+    private readonly variableName: string
+  ) {
+    super();
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    const edit = this.expressionEditingService.insertText(
+      currentState.expressionText,
+      this.variableName,
+      currentState.selectionStart,
+      currentState.selectionEnd
+    );
+
+    return currentState.copyWith({
+      expressionText: edit.text,
+      cursorPosition: edit.cursorPosition,
+      selectionStart: edit.selectionStart,
+      selectionEnd: edit.selectionEnd,
+      resultText: null,
+      errorText: null,
+    });
+  }
+}
