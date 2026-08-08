c29c55b6a24b23682dd1cb4154f4f348de169220
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:30:40 +0800

    feat: add calculator session orchestration service contract

---
+++ src/application/services/CalculatorSessionOrchestrationService.ts
@@ -0,0 +1,15 @@
+import { CalculatorSettings } from "../../domain/model/CalculatorSettings";
+import { HistoryEntry } from "../../domain/model/HistoryEntry";
+import { VariableAssignment } from "../../domain/model/VariableAssignment";
+
+export interface CalculatorSessionOrchestrationService {
+  recordHistoryEntry(entry: HistoryEntry): Promise<void>;
+  refreshHistoryEntries(): Promise<readonly HistoryEntry[]>;
+  removeHistoryEntry(historyEntryId: string): Promise<void>;
+  clearHistoryEntries(): Promise<void>;
+  refreshVariables(): Promise<readonly VariableAssignment[]>;
+  saveVariable(variable: VariableAssignment): Promise<void>;
+  removeVariable(variableName: string): Promise<void>;
+  loadSettings(): CalculatorSettings | null;
+  saveSettings(settings: CalculatorSettings): void;
+}
