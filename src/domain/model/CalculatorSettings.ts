12e02a1aa8fb34ed6fb926ec1067bbbd3cb03545
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 08:51:38 +0800

    feat: add calculator settings model

---
+++ src/domain/model/CalculatorSettings.ts
@@ -0,0 +1,23 @@
+import { AngleMode } from "./AngleMode";
+import { NumericMode } from "./NumericMode";
+import { ThemePreference } from "./ThemePreference";
+
+export class CalculatorSettings {
+  public constructor(
+    public readonly angleMode: AngleMode,
+    public readonly numericMode: NumericMode,
+    public readonly complexNumbersEnabled: boolean,
+    public readonly casEnabled: boolean,
+    public readonly themePreference: ThemePreference
+  ) {}
+
+  public static createDefault(): CalculatorSettings {
+    return new CalculatorSettings(
+      AngleMode.DEG,
+      NumericMode.STANDARD,
+      false,
+      false,
+      ThemePreference.SYSTEM
+    );
+  }
+}
