34db0324e3aa50ef451d30dc1182947e36c6effe
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:19 +0800

    style: format src/application/calculus/CalculusExpressionEvaluator.ts with oxfmt

--- src/application/calculus/CalculusExpressionEvaluator.ts
+++ src/application/calculus/CalculusExpressionEvaluator.ts
@@ -1,10 +1,10 @@
 import { AngleMode } from "../../domain/model/AngleMode";
 
 export interface CalculusExpressionEvaluator {
-  evaluate(
-    expressionText: string,
-    variableName: string,
-    value: number,
-    angleMode: AngleMode
-  ): number;
+    evaluate(
+        expressionText: string,
+        variableName: string,
+        value: number,
+        angleMode: AngleMode,
+    ): number;
 }
