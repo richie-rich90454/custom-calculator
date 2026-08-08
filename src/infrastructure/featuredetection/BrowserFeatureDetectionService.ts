16746b4e6684ff6ff04c1577d9284cb003fbf4a6
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:05:24 +0800

    feat: add browser feature detection service contract

---
+++ src/infrastructure/featuredetection/BrowserFeatureDetectionService.ts
@@ -0,0 +1,7 @@
+export interface BrowserFeatureSnapshot {
+  readonly bigIntSupported: boolean;
+}
+
+export interface BrowserFeatureDetectionService {
+  detectBrowserFeatures(): BrowserFeatureSnapshot;
+}
