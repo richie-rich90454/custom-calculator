0254b77f6b0b77afbea41d9a308a3491e5e133e6
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:53:00 +0800

    style: format src/presentation/viewmodels/CalculatorViewModelMapper.ts with oxfmt

--- src/presentation/viewmodels/CalculatorViewModelMapper.ts
+++ src/presentation/viewmodels/CalculatorViewModelMapper.ts
@@ -3,57 +3,55 @@ import { CalculatorSettings } from "../../domain/model/CalculatorSettings";
 import type { CalculatorUiState } from "../../state/CalculatorUiState";
 
 export class CalculatorViewModelMapper {
-  public mapUiStateToSessionState(
-    uiState: CalculatorUiState
-  ): CalculatorSessionState {
-    return new CalculatorSessionState(
-      uiState.expressionText,
-      uiState.cursorPosition,
-      uiState.selectionStart,
-      uiState.selectionEnd,
-      uiState.resultText,
-      uiState.errorText,
-      uiState.lastResultText,
-      uiState.lastResultValue,
-      uiState.angleMode,
-      uiState.numericMode,
-      uiState.complexNumbersEnabled,
-      uiState.casEnabled,
-      uiState.variables,
-      uiState.memoryValueText
-    );
-  }
+    public mapUiStateToSessionState(uiState: CalculatorUiState): CalculatorSessionState {
+        return new CalculatorSessionState(
+            uiState.expressionText,
+            uiState.cursorPosition,
+            uiState.selectionStart,
+            uiState.selectionEnd,
+            uiState.resultText,
+            uiState.errorText,
+            uiState.lastResultText,
+            uiState.lastResultValue,
+            uiState.angleMode,
+            uiState.numericMode,
+            uiState.complexNumbersEnabled,
+            uiState.casEnabled,
+            uiState.variables,
+            uiState.memoryValueText,
+        );
+    }
 
-  public mapSessionStateToUiState(
-    sessionState: CalculatorSessionState,
-    currentUiState: CalculatorUiState
-  ): CalculatorUiState {
-    return {
-      ...currentUiState,
-      expressionText: sessionState.expressionText,
-      cursorPosition: sessionState.cursorPosition,
-      selectionStart: sessionState.selectionStart,
-      selectionEnd: sessionState.selectionEnd,
-      resultText: sessionState.resultText,
-      errorText: sessionState.errorText,
-      lastResultText: sessionState.lastResultText,
-      lastResultValue: sessionState.lastResultValue,
-      angleMode: sessionState.angleMode,
-      numericMode: sessionState.numericMode,
-      complexNumbersEnabled: sessionState.complexNumbersEnabled,
-      casEnabled: sessionState.casEnabled,
-      variables: sessionState.variables,
-      memoryValueText: sessionState.memoryValueText,
-    };
-  }
+    public mapSessionStateToUiState(
+        sessionState: CalculatorSessionState,
+        currentUiState: CalculatorUiState,
+    ): CalculatorUiState {
+        return {
+            ...currentUiState,
+            expressionText: sessionState.expressionText,
+            cursorPosition: sessionState.cursorPosition,
+            selectionStart: sessionState.selectionStart,
+            selectionEnd: sessionState.selectionEnd,
+            resultText: sessionState.resultText,
+            errorText: sessionState.errorText,
+            lastResultText: sessionState.lastResultText,
+            lastResultValue: sessionState.lastResultValue,
+            angleMode: sessionState.angleMode,
+            numericMode: sessionState.numericMode,
+            complexNumbersEnabled: sessionState.complexNumbersEnabled,
+            casEnabled: sessionState.casEnabled,
+            variables: sessionState.variables,
+            memoryValueText: sessionState.memoryValueText,
+        };
+    }
 
-  public mapUiStateToSettings(uiState: CalculatorUiState): CalculatorSettings {
-    return new CalculatorSettings(
-      uiState.angleMode,
-      uiState.numericMode,
-      uiState.complexNumbersEnabled,
-      uiState.casEnabled,
-      uiState.themePreference
-    );
-  }
+    public mapUiStateToSettings(uiState: CalculatorUiState): CalculatorSettings {
+        return new CalculatorSettings(
+            uiState.angleMode,
+            uiState.numericMode,
+            uiState.complexNumbersEnabled,
+            uiState.casEnabled,
+            uiState.themePreference,
+        );
+    }
 }
