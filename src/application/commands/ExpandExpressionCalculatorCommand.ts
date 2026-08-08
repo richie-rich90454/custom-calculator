4192cb7581a7b567704618db5768f8bf5dd6e824
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:38 +0800

    style: format src/application/commands/ExpandExpressionCalculatorCommand.ts with oxfmt

--- src/application/commands/ExpandExpressionCalculatorCommand.ts
+++ src/application/commands/ExpandExpressionCalculatorCommand.ts
@@ -3,15 +3,13 @@ import type { CasService } from "../../domain/services/CasService";
 import { AbstractCasCalculatorCommand } from "./AbstractCasCalculatorCommand";
 
 export class ExpandExpressionCalculatorCommand extends AbstractCasCalculatorCommand {
-  public constructor(casService: CasService) {
-    super(casService);
-  }
+    public constructor(casService: CasService) {
+        super(casService);
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    return this.executeCasOperation(currentState, (casService) =>
-      casService.expandExpression(currentState.expressionText)
-    );
-  }
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        return this.executeCasOperation(currentState, (casService) =>
+            casService.expandExpression(currentState.expressionText),
+        );
+    }
 }
