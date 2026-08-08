c7b32ea3aede771ffadb9b5b029dfabc63fd6dcd
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:53:00 +0800

    style: format src/presentation/viewmodels/CalculatorViewModel.ts with oxfmt

--- src/presentation/viewmodels/CalculatorViewModel.ts
+++ src/presentation/viewmodels/CalculatorViewModel.ts
@@ -9,92 +9,92 @@ import type { ScientificFunctionDefinition } from "../../domain/services/Scienti
 import type { CalculatorPanelName, CalculatorUiState } from "../../state/CalculatorUiState";
 
 export class CalculatorViewModel {
-  public constructor(
-    private readonly uiState: CalculatorUiState,
-    private readonly compositionRoot: CalculatorCompositionRoot
-  ) {}
+    public constructor(
+        private readonly uiState: CalculatorUiState,
+        private readonly compositionRoot: CalculatorCompositionRoot,
+    ) {}
 
-  public get expressionText(): string {
-    return this.uiState.expressionText;
-  }
+    public get expressionText(): string {
+        return this.uiState.expressionText;
+    }
 
-  public get cursorPosition(): number {
-    return this.uiState.cursorPosition;
-  }
+    public get cursorPosition(): number {
+        return this.uiState.cursorPosition;
+    }
 
-  public get selectionStart(): number {
-    return this.uiState.selectionStart;
-  }
+    public get selectionStart(): number {
+        return this.uiState.selectionStart;
+    }
 
-  public get selectionEnd(): number {
-    return this.uiState.selectionEnd;
-  }
+    public get selectionEnd(): number {
+        return this.uiState.selectionEnd;
+    }
 
-  public get resultText(): string | null {
-    return this.uiState.resultText;
-  }
+    public get resultText(): string | null {
+        return this.uiState.resultText;
+    }
 
-  public get errorText(): string | null {
-    return this.uiState.errorText;
-  }
+    public get errorText(): string | null {
+        return this.uiState.errorText;
+    }
 
-  public get angleMode(): AngleMode {
-    return this.uiState.angleMode;
-  }
+    public get angleMode(): AngleMode {
+        return this.uiState.angleMode;
+    }
 
-  public get numericMode(): NumericMode {
-    return this.uiState.numericMode;
-  }
+    public get numericMode(): NumericMode {
+        return this.uiState.numericMode;
+    }
 
-  public get complexNumbersEnabled(): boolean {
-    return this.uiState.complexNumbersEnabled;
-  }
+    public get complexNumbersEnabled(): boolean {
+        return this.uiState.complexNumbersEnabled;
+    }
 
-  public get casEnabled(): boolean {
-    return this.uiState.casEnabled;
-  }
+    public get casEnabled(): boolean {
+        return this.uiState.casEnabled;
+    }
 
-  public get bigIntSupported(): boolean {
-    return this.uiState.bigIntSupported;
-  }
+    public get bigIntSupported(): boolean {
+        return this.uiState.bigIntSupported;
+    }
 
-  public get themePreference(): ThemePreference {
-    return this.uiState.themePreference;
-  }
+    public get themePreference(): ThemePreference {
+        return this.uiState.themePreference;
+    }
 
-  public get memoryValueText(): string | null {
-    return this.uiState.memoryValueText;
-  }
+    public get memoryValueText(): string | null {
+        return this.uiState.memoryValueText;
+    }
 
-  public get activePanel(): CalculatorPanelName {
-    return this.uiState.activePanel;
-  }
+    public get activePanel(): CalculatorPanelName {
+        return this.uiState.activePanel;
+    }
 
-  public get statusMessage(): string | null {
-    return this.uiState.statusMessage;
-  }
+    public get statusMessage(): string | null {
+        return this.uiState.statusMessage;
+    }
 
-  public get historyEntries(): readonly HistoryEntry[] {
-    return this.uiState.historyEntries;
-  }
+    public get historyEntries(): readonly HistoryEntry[] {
+        return this.uiState.historyEntries;
+    }
 
-  public get variables(): readonly VariableAssignment[] {
-    return this.uiState.variables;
-  }
+    public get variables(): readonly VariableAssignment[] {
+        return this.uiState.variables;
+    }
 
-  public get functions(): readonly ScientificFunctionDefinition[] {
-    return this.compositionRoot.functionCatalogService.getAllFunctions();
-  }
+    public get functions(): readonly ScientificFunctionDefinition[] {
+        return this.compositionRoot.functionCatalogService.getAllFunctions();
+    }
 
-  public get constants(): readonly ScientificConstant[] {
-    return this.compositionRoot.constantCatalogService.getAllConstants();
-  }
+    public get constants(): readonly ScientificConstant[] {
+        return this.compositionRoot.constantCatalogService.getAllConstants();
+    }
 
-  public get supportedNumericModes(): readonly NumericMode[] {
-    return this.compositionRoot.numericModePolicyService.getSupportedNumericModes();
-  }
+    public get supportedNumericModes(): readonly NumericMode[] {
+        return this.compositionRoot.numericModePolicyService.getSupportedNumericModes();
+    }
 
-  public get hasMemory(): boolean {
-    return this.uiState.memoryValueText !== null;
-  }
+    public get hasMemory(): boolean {
+        return this.uiState.memoryValueText !== null;
+    }
 }
