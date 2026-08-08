225f441cfda8dc5088a30b335725d040bd1e0d6e
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:22:21 +0800

    feat: add insert function calculator command

---
+++ src/application/commands/InsertFunctionCalculatorCommand.ts
@@ -0,0 +1,34 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { ExpressionEditingService } from "../../domain/services/ExpressionEditingService";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class InsertFunctionCalculatorCommand extends AbstractCalculatorCommand {
+  public constructor(
+    private readonly expressionEditingService: ExpressionEditingService,
+    private readonly functionName: string
+  ) {
+    super();
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    const functionInvocationText = `${this.functionName}(`;
+
+    const edit = this.expressionEditingService.insertText(
+      currentState.expressionText,
+      functionInvocationText,
+      currentState.selectionStart,
+      currentState.selectionEnd
+    );
+
+    return currentState.copyWith({
+      expressionText: edit.text,
+      cursorPosition: edit.cursorPosition,
+      selectionStart: edit.selectionStart,
+      selectionEnd: edit.selectionEnd,
+      resultText: null,
+      errorText: null,
+    });
+  }
+}
