9589382d4cc9476e7ce4af6f890f494c1dae4d37
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:30 +0800

    style: format src/presentation/primitives/AccessibleSwitchComponent.tsx with oxfmt

--- src/presentation/primitives/AccessibleSwitchComponent.tsx
+++ src/presentation/primitives/AccessibleSwitchComponent.tsx
@@ -3,23 +3,20 @@ import { Switch, type SwitchProps } from "react-aria-components";
 import { cssClass } from "../utils/classNames";
 import styles from "../styles/AccessibleSwitchComponent.module.css";
 
-export interface AccessibleSwitchComponentProperties
-  extends Omit<SwitchProps, "className"> {
-  readonly label: string;
-  readonly children?: ReactNode;
+export interface AccessibleSwitchComponentProperties extends Omit<SwitchProps, "className"> {
+    readonly label: string;
+    readonly children?: ReactNode;
 }
 
-export function AccessibleSwitchComponent(
-  props: AccessibleSwitchComponentProperties
-) {
-  const { label, children, ...switchProperties } = props;
+export function AccessibleSwitchComponent(props: AccessibleSwitchComponentProperties) {
+    const { label, children, ...switchProperties } = props;
 
-  return (
-    <Switch {...switchProperties} className={cssClass(styles.switch)}>
-      <span className={cssClass(styles.track)}>
-        <span className={cssClass(styles.thumb)} />
-      </span>
-      <span className={cssClass(styles.label)}>{children ?? label}</span>
-    </Switch>
-  );
+    return (
+        <Switch {...switchProperties} className={cssClass(styles.switch)}>
+            <span className={cssClass(styles.track)}>
+                <span className={cssClass(styles.thumb)} />
+            </span>
+            <span className={cssClass(styles.label)}>{children ?? label}</span>
+        </Switch>
+    );
 }
