025dbb37f39cf46a1e61e119151187f005560a7f
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:41 +0800

    style: format src/application/commands/SimplifyExpressionCalculatorCommand.ts with oxfmt

--- src/application/commands/SimplifyExpressionCalculatorCommand.ts
+++ src/application/commands/SimplifyExpressionCalculatorCommand.ts
@@ -3,15 +3,13 @@ import type { CasService } from "../../domain/services/CasService";
 import { AbstractCasCalculatorCommand } from "./AbstractCasCalculatorCommand";
 
 export class SimplifyExpressionCalculatorCommand extends AbstractCasCalculatorCommand {
-  public constructor(casService: CasService) {
-    super(casService);
-  }
+    public constructor(casService: CasService) {
+        super(casService);
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    return this.executeCasOperation(currentState, (casService) =>
-      casService.simplifyExpression(currentState.expressionText)
-    );
-  }
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        return this.executeCasOperation(currentState, (casService) =>
+            casService.simplifyExpression(currentState.expressionText),
+        );
+    }
 }
