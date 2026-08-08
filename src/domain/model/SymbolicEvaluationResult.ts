c3e8d78379db3744ee91c54d800ddade95e2ec2c
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 16:59:58 +0800

    feat: add symbolic calculus evaluation result model

---
+++ src/domain/model/SymbolicEvaluationResult.ts
@@ -0,0 +1,11 @@
+import { EvaluationResult } from "./EvaluationResult";
+
+export class SymbolicEvaluationResult extends EvaluationResult {
+  public constructor(
+    public readonly symbolicText: string,
+    public readonly latexText: string,
+    public readonly displayText: string
+  ) {
+    super(symbolicText, displayText, symbolicText);
+  }
+}
