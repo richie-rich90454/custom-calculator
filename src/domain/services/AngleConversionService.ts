8d5c20eb30526e4bb0742d6951e4194f46936630
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 08:52:50 +0800

    feat: add angle conversion service contract

---
+++ src/domain/services/AngleConversionService.ts
@@ -0,0 +1,6 @@
+import { AngleMode } from "../model/AngleMode";
+
+export interface AngleConversionService {
+  convertToRadians(value: number, angleMode: AngleMode): number;
+  convertFromRadians(value: number, angleMode: AngleMode): number;
+}
