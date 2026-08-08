f7c50953d927115c24afbaade15adc0400c03e63
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:45 +0800

    style: format src/domain/model/HistoryEntry.ts with oxfmt

--- src/domain/model/HistoryEntry.ts
+++ src/domain/model/HistoryEntry.ts
@@ -2,13 +2,13 @@ import { AngleMode } from "./AngleMode";
 import { NumericMode } from "./NumericMode";
 
 export class HistoryEntry {
-  public constructor(
-    public readonly id: string,
-    public readonly expressionText: string,
-    public readonly resultText: string,
-    public readonly angleMode: AngleMode,
-    public readonly numericMode: NumericMode,
-    public readonly complexNumbersEnabled: boolean,
-    public readonly createdAt: string
-  ) {}
+    public constructor(
+        public readonly id: string,
+        public readonly expressionText: string,
+        public readonly resultText: string,
+        public readonly angleMode: AngleMode,
+        public readonly numericMode: NumericMode,
+        public readonly complexNumbersEnabled: boolean,
+        public readonly createdAt: string,
+    ) {}
 }
