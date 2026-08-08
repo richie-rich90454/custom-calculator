b45392c716a2c4361c21742522ab7dfca058e08f
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:24 +0800

    style: format src/application/calculus/SymbolicDifferentiationService.ts with oxfmt

--- src/application/calculus/SymbolicDifferentiationService.ts
+++ src/application/calculus/SymbolicDifferentiationService.ts
@@ -2,10 +2,10 @@ import { AngleMode } from "../../domain/model/AngleMode";
 import { CalculusAngleModePolicy } from "./CalculusAngleModePolicy";
 
 export interface SymbolicDifferentiationService {
-  differentiateSymbolically(
-    expressionText: string,
-    variableName: string,
-    angleMode: AngleMode,
-    policy: CalculusAngleModePolicy
-  ): string;
+    differentiateSymbolically(
+        expressionText: string,
+        variableName: string,
+        angleMode: AngleMode,
+        policy: CalculusAngleModePolicy,
+    ): string;
 }
