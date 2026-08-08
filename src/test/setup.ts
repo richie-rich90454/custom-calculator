c1c9cada6e31db4e48daa21c50f3b4f17ace0254
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:53:02 +0800

    style: format src/test/setup.ts with oxfmt

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,15 +1,15 @@
 import "@testing-library/jest-dom/vitest";
 
 Object.defineProperty(globalThis, "matchMedia", {
-  writable: true,
-  value: (query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addEventListener: () => undefined,
-    removeEventListener: () => undefined,
-    addListener: () => undefined,
-    removeListener: () => undefined,
-    dispatchEvent: () => false,
-  }),
+    writable: true,
+    value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: () => undefined,
+        removeEventListener: () => undefined,
+        addListener: () => undefined,
+        removeListener: () => undefined,
+        dispatchEvent: () => false,
+    }),
 });
