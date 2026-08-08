8d4e6dc0217a889c32efeb6e313c5cb39b8aad5c
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:18 +0800

    style: format src/infrastructure/persistence/IndexedDbVariablesRepository.ts with oxfmt

--- src/infrastructure/persistence/IndexedDbVariablesRepository.ts
+++ src/infrastructure/persistence/IndexedDbVariablesRepository.ts
@@ -3,27 +3,23 @@ import type { VariablesRepository } from "../../domain/repositories/VariablesRep
 import { CalculatorDexieDatabase } from "./CalculatorDexieDatabase";
 
 export class IndexedDbVariablesRepository implements VariablesRepository {
-  public constructor(private readonly database: CalculatorDexieDatabase) {}
+    public constructor(private readonly database: CalculatorDexieDatabase) {}
 
-  public async loadVariables(): Promise<readonly VariableAssignment[]> {
-    const records = await this.database.variables.toArray();
+    public async loadVariables(): Promise<readonly VariableAssignment[]> {
+        const records = await this.database.variables.toArray();
 
-    return records.map((record) =>
-      this.database.fromVariableAssignmentRecord(record)
-    );
-  }
+        return records.map((record) => this.database.fromVariableAssignmentRecord(record));
+    }
 
-  public async saveVariable(variable: VariableAssignment): Promise<void> {
-    await this.database.variables.put(
-      this.database.toVariableAssignmentRecord(variable)
-    );
-  }
+    public async saveVariable(variable: VariableAssignment): Promise<void> {
+        await this.database.variables.put(this.database.toVariableAssignmentRecord(variable));
+    }
 
-  public async deleteVariable(variableName: string): Promise<void> {
-    await this.database.variables.delete(variableName);
-  }
+    public async deleteVariable(variableName: string): Promise<void> {
+        await this.database.variables.delete(variableName);
+    }
 
-  public async clearVariables(): Promise<void> {
-    await this.database.variables.clear();
-  }
+    public async clearVariables(): Promise<void> {
+        await this.database.variables.clear();
+    }
 }
