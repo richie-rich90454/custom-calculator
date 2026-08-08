59814373ab09081a14b7f10a1ea011e189226ed9
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 17:01:29 +0800

    feat: add calculus angle mode policy service contract

---
+++ src/application/calculus/CalculusAngleModePolicyService.ts
@@ -0,0 +1,15 @@
+import { AngleMode } from "../../domain/model/AngleMode";
+import { CalculusAngleModePolicy } from "./CalculusAngleModePolicy";
+
+export interface CalculusAngleModePolicyService {
+  getRadiansConversionFactor(angleMode: AngleMode): number;
+  getSymbolicTrigConversionFactor(
+    angleMode: AngleMode,
+    policy: CalculusAngleModePolicy
+  ): number;
+  resolveSymbolicAngleWarning(
+    angleMode: AngleMode,
+    policy: CalculusAngleModePolicy
+  ): string | null;
+  resolveNumericAngleMode(angleMode: AngleMode): AngleMode;
+}
