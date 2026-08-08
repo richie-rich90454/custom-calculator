3d9f5e68dc745049ce554e8dffccf41283658990
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:31 +0800

    style: format src/presentation/primitives/PanelSectionComponent.tsx with oxfmt

--- src/presentation/primitives/PanelSectionComponent.tsx
+++ src/presentation/primitives/PanelSectionComponent.tsx
@@ -3,20 +3,18 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/PanelPrimitives.module.css";
 
 interface PanelSectionComponentProperties {
-  readonly heading: string;
-  readonly children: ReactNode;
+    readonly heading: string;
+    readonly children: ReactNode;
 }
 
 /**
  * Consistent titled section used to group content inside a panel.
  */
-export function PanelSectionComponent(
-  props: PanelSectionComponentProperties
-) {
-  return (
-    <section className={cssClass(styles.section)}>
-      <h3 className={cssClass(styles.sectionHeading)}>{props.heading}</h3>
-      {props.children}
-    </section>
-  );
+export function PanelSectionComponent(props: PanelSectionComponentProperties) {
+    return (
+        <section className={cssClass(styles.section)}>
+            <h3 className={cssClass(styles.sectionHeading)}>{props.heading}</h3>
+            {props.children}
+        </section>
+    );
 }
