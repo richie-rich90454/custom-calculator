b79a463fc916d1c31ae80c9e42a8cac54888cd41
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:34:35 +0800

    feat: add calculator UI state model

---
+++ src/state/CalculatorUiState.ts
@@ -0,0 +1,35 @@
+import { AngleMode } from "../domain/model/AngleMode";
+import { HistoryEntry } from "../domain/model/HistoryEntry";
+import { NumericMode } from "../domain/model/NumericMode";
+import { ThemePreference } from "../domain/model/ThemePreference";
+import { VariableAssignment } from "../domain/model/VariableAssignment";
+
+export enum CalculatorPanelName {
+  NONE = "NONE",
+  HISTORY = "HISTORY",
+  CONSTANTS = "CONSTANTS",
+  VARIABLES = "VARIABLES",
+  MEMORY = "MEMORY",
+  SETTINGS = "SETTINGS",
+  CAS = "CAS",
+}
+
+export interface CalculatorUiState {
+  expressionText: string;
+  cursorPosition: number;
+  selectionStart: number;
+  selectionEnd: number;
+  resultText: string | null;
+  errorText: string | null;
+  angleMode: AngleMode;
+  numericMode: NumericMode;
+  complexNumbersEnabled: boolean;
+  casEnabled: boolean;
+  bigIntSupported: boolean;
+  themePreference: ThemePreference;
+  historyEntries: readonly HistoryEntry[];
+  variables: readonly VariableAssignment[];
+  memoryValueText: string | null;
+  activePanel: CalculatorPanelName;
+  statusMessage: string | null;
+}
