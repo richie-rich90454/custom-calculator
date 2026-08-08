ad1883c3795394713173466f51852954dc1dcade
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:25 +0800

    style: format src/presentation/components/ConstantsPanelSearchComponent.tsx with oxfmt

--- src/presentation/components/ConstantsPanelSearchComponent.tsx
+++ src/presentation/components/ConstantsPanelSearchComponent.tsx
@@ -1,18 +1,16 @@
 import { AccessibleTextFieldComponent } from "../primitives/AccessibleTextFieldComponent";
 
 interface ConstantsPanelSearchComponentProperties {
-  readonly searchText: string;
-  readonly onSearchTextChanged: (searchText: string) => void;
+    readonly searchText: string;
+    readonly onSearchTextChanged: (searchText: string) => void;
 }
 
-export function ConstantsPanelSearchComponent(
-  props: ConstantsPanelSearchComponentProperties
-) {
-  return (
-    <AccessibleTextFieldComponent
-      label="Search constants"
-      value={props.searchText}
-      onChange={props.onSearchTextChanged}
-    />
-  );
+export function ConstantsPanelSearchComponent(props: ConstantsPanelSearchComponentProperties) {
+    return (
+        <AccessibleTextFieldComponent
+            label="Search constants"
+            value={props.searchText}
+            onChange={props.onSearchTextChanged}
+        />
+    );
 }
