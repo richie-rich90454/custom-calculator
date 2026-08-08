f202220e1e9a2281b53104f9aeaf00049a0a54c2
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:24 +0800

    style: format src/application/calculus/NumericIntegrationService.ts with oxfmt

--- src/application/calculus/NumericIntegrationService.ts
+++ src/application/calculus/NumericIntegrationService.ts
@@ -1,11 +1,11 @@
 import { AngleMode } from "../../domain/model/AngleMode";
 
 export interface NumericIntegrationService {
-  integrate(
-    expressionText: string,
-    variableName: string,
-    lowerBound: number,
-    upperBound: number,
-    angleMode: AngleMode
-  ): number;
+    integrate(
+        expressionText: string,
+        variableName: string,
+        lowerBound: number,
+        upperBound: number,
+        angleMode: AngleMode,
+    ): number;
 }
