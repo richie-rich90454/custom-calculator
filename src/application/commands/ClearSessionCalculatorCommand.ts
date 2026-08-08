27056d66a989cb22ebc4322e5ea1d13b1f0b76b5
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:23:38 +0800

    feat: add clear session calculator command

---
+++ src/application/commands/ClearSessionCalculatorCommand.ts
@@ -0,0 +1,17 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class ClearSessionCalculatorCommand extends AbstractCalculatorCommand {
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    return currentState.copyWith({
+      expressionText: "",
+      cursorPosition: 0,
+      selectionStart: 0,
+      selectionEnd: 0,
+      resultText: null,
+      errorText: null,
+    });
+  }
+}
