f8b6685f1083552af064ed4e52bd4e2baa15b5d7
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:37 +0800

    style: format src/application/commands/DifferentiateExpressionCalculatorCommand.ts with oxfmt

--- src/application/commands/DifferentiateExpressionCalculatorCommand.ts
+++ src/application/commands/DifferentiateExpressionCalculatorCommand.ts
@@ -3,21 +3,16 @@ import type { CasService } from "../../domain/services/CasService";
 import { AbstractCasCalculatorCommand } from "./AbstractCasCalculatorCommand";
 
 export class DifferentiateExpressionCalculatorCommand extends AbstractCasCalculatorCommand {
-  public constructor(
-    casService: CasService,
-    private readonly variableName: string
-  ) {
-    super(casService);
-  }
+    public constructor(
+        casService: CasService,
+        private readonly variableName: string,
+    ) {
+        super(casService);
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    return this.executeCasOperation(currentState, (casService) =>
-      casService.differentiateExpression(
-        currentState.expressionText,
-        this.variableName
-      )
-    );
-  }
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        return this.executeCasOperation(currentState, (casService) =>
+            casService.differentiateExpression(currentState.expressionText, this.variableName),
+        );
+    }
 }
