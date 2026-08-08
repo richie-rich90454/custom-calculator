042b814d198b66a7cf38eddd55b68f8a9c38a4e7
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:25:45 +0800

    feat: add memory subtract calculator command

---
+++ src/application/commands/MemorySubtractCalculatorCommand.ts
@@ -0,0 +1,35 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { ExpressionEvaluationGateway } from "../../domain/services/ExpressionEvaluationGateway";
+import { AbstractMemoryCalculatorCommand } from "./AbstractMemoryCalculatorCommand";
+
+export class MemorySubtractCalculatorCommand extends AbstractMemoryCalculatorCommand {
+  public constructor(
+    expressionEvaluationGateway: ExpressionEvaluationGateway
+  ) {
+    super(expressionEvaluationGateway);
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    if (currentState.resultText === null) {
+      return currentState;
+    }
+
+    const evaluationResult = this.evaluateMemoryExpression(
+      currentState,
+      currentState.memoryValueText,
+      currentState.resultText,
+      "-"
+    );
+
+    if (evaluationResult === null) {
+      return currentState;
+    }
+
+    return currentState.copyWith({
+      memoryValueText: evaluationResult.resultText,
+      errorText: null,
+    });
+  }
+}
