83d2403be235d40805533ccfe1c64d10451e7daf
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:23 +0800

    style: format src/application/calculus/FiniteSummationService.ts with oxfmt

--- src/application/calculus/FiniteSummationService.ts
+++ src/application/calculus/FiniteSummationService.ts
@@ -1,11 +1,11 @@
 import { AngleMode } from "../../domain/model/AngleMode";
 
 export interface FiniteSummationService {
-  sum(
-    expressionText: string,
-    variableName: string,
-    lowerBound: number,
-    upperBound: number,
-    angleMode: AngleMode
-  ): number;
+    sum(
+        expressionText: string,
+        variableName: string,
+        lowerBound: number,
+        upperBound: number,
+        angleMode: AngleMode,
+    ): number;
 }
