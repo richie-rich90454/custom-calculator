474d51c7246027550d2000892ad439e388bcacb8
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:24 +0800

    style: format src/presentation/components/ConstantsPanelCategorySelectorComponent.tsx with oxfmt

--- src/presentation/components/ConstantsPanelCategorySelectorComponent.tsx
+++ src/presentation/components/ConstantsPanelCategorySelectorComponent.tsx
@@ -1,33 +1,31 @@
 import {
-  AccessibleSelectComponent,
-  type AccessibleSelectOption,
+    AccessibleSelectComponent,
+    type AccessibleSelectOption,
 } from "../primitives/AccessibleSelectComponent";
 import type { ConstantsPanelCategoryOption } from "../viewmodels/ConstantsPanelViewModel";
 
 interface ConstantsPanelCategorySelectorComponentProperties {
-  readonly options: readonly ConstantsPanelCategoryOption[];
-  readonly selectedCategoryId: string;
-  readonly onCategoryChanged: (categoryId: string) => void;
+    readonly options: readonly ConstantsPanelCategoryOption[];
+    readonly selectedCategoryId: string;
+    readonly onCategoryChanged: (categoryId: string) => void;
 }
 
 export function ConstantsPanelCategorySelectorComponent(
-  props: ConstantsPanelCategorySelectorComponentProperties
+    props: ConstantsPanelCategorySelectorComponentProperties,
 ) {
-  const selectOptions: readonly AccessibleSelectOption[] = props.options.map(
-    (option) => ({
-      id: option.id,
-      label: option.label,
-    })
-  );
+    const selectOptions: readonly AccessibleSelectOption[] = props.options.map((option) => ({
+        id: option.id,
+        label: option.label,
+    }));
 
-  return (
-    <AccessibleSelectComponent
-      label="Constant category"
-      options={selectOptions}
-      selectedKey={props.selectedCategoryId}
-      onSelectionChange={(key) => {
-        props.onCategoryChanged(String(key));
-      }}
-    />
-  );
+    return (
+        <AccessibleSelectComponent
+            label="Constant category"
+            options={selectOptions}
+            selectedKey={props.selectedCategoryId}
+            onSelectionChange={(key) => {
+                props.onCategoryChanged(String(key));
+            }}
+        />
+    );
 }
