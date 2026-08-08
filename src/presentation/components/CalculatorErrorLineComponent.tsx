e588a632583b76e2b29263a6b5fe5cff152de26a
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:20 +0800

    style: format src/presentation/components/CalculatorErrorLineComponent.tsx with oxfmt

--- src/presentation/components/CalculatorErrorLineComponent.tsx
+++ src/presentation/components/CalculatorErrorLineComponent.tsx
@@ -2,21 +2,19 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/CalculatorErrorLineComponent.module.css";
 
 interface CalculatorErrorLineComponentProperties {
-  readonly errorText: string | null;
+    readonly errorText: string | null;
 }
 
-export function CalculatorErrorLineComponent(
-  props: CalculatorErrorLineComponentProperties
-) {
-  const { errorText } = props;
+export function CalculatorErrorLineComponent(props: CalculatorErrorLineComponentProperties) {
+    const { errorText } = props;
 
-  if (errorText === null) {
-    return null;
-  }
+    if (errorText === null) {
+        return null;
+    }
 
-  return (
-    <p id="error-text" className={cssClass(styles.error)} role="alert">
-      {errorText}
-    </p>
-  );
+    return (
+        <p id="error-text" className={cssClass(styles.error)} role="alert">
+            {errorText}
+        </p>
+    );
 }
