07b4c20e6ee4389f6dc7bde7c82e53d4c8d4f662
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:42 +0800

    style: format src/application/controllers/CalculatorApplicationController.ts with oxfmt

--- src/application/controllers/CalculatorApplicationController.ts
+++ src/application/controllers/CalculatorApplicationController.ts
@@ -3,101 +3,85 @@ import { NumericMode } from "../../domain/model/NumericMode";
 import type { ExpressionTextEdit } from "../../domain/services/ExpressionEditingService";
 
 export interface CalculatorApplicationController {
-  applyInsertion(
-    currentState: CalculatorSessionState,
-    edit: ExpressionTextEdit
-  ): CalculatorSessionState;
+    applyInsertion(
+        currentState: CalculatorSessionState,
+        edit: ExpressionTextEdit,
+    ): CalculatorSessionState;
 
-  insertDigit(
-    currentState: CalculatorSessionState,
-    digit: string
-  ): CalculatorSessionState;
+    insertDigit(currentState: CalculatorSessionState, digit: string): CalculatorSessionState;
 
-  insertOperator(
-    currentState: CalculatorSessionState,
-    operator: string
-  ): CalculatorSessionState;
+    insertOperator(currentState: CalculatorSessionState, operator: string): CalculatorSessionState;
 
-  insertFunction(
-    currentState: CalculatorSessionState,
-    functionName: string
-  ): CalculatorSessionState;
+    insertFunction(
+        currentState: CalculatorSessionState,
+        functionName: string,
+    ): CalculatorSessionState;
 
-  insertConstant(
-    currentState: CalculatorSessionState,
-    constantId: string
-  ): CalculatorSessionState;
+    insertConstant(
+        currentState: CalculatorSessionState,
+        constantId: string,
+    ): CalculatorSessionState;
 
-  insertVariable(
-    currentState: CalculatorSessionState,
-    variableName: string
-  ): CalculatorSessionState;
+    insertVariable(
+        currentState: CalculatorSessionState,
+        variableName: string,
+    ): CalculatorSessionState;
 
-  insertParenthesis(
-    currentState: CalculatorSessionState,
-    parenthesis: string
-  ): CalculatorSessionState;
+    insertParenthesis(
+        currentState: CalculatorSessionState,
+        parenthesis: string,
+    ): CalculatorSessionState;
 
-  deleteBackward(currentState: CalculatorSessionState): CalculatorSessionState;
+    deleteBackward(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  deleteForward(currentState: CalculatorSessionState): CalculatorSessionState;
+    deleteForward(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  deleteWordBackward(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState;
+    deleteWordBackward(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  clearSession(currentState: CalculatorSessionState): CalculatorSessionState;
+    clearSession(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  setExpressionText(
-    currentState: CalculatorSessionState,
-    expressionText: string
-  ): CalculatorSessionState;
+    setExpressionText(
+        currentState: CalculatorSessionState,
+        expressionText: string,
+    ): CalculatorSessionState;
 
-  evaluateExpression(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState;
+    evaluateExpression(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  cycleAngleMode(currentState: CalculatorSessionState): CalculatorSessionState;
+    cycleAngleMode(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  changeNumericMode(
-    currentState: CalculatorSessionState,
-    numericMode: NumericMode
-  ): CalculatorSessionState;
+    changeNumericMode(
+        currentState: CalculatorSessionState,
+        numericMode: NumericMode,
+    ): CalculatorSessionState;
 
-  toggleComplexNumbers(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState;
+    toggleComplexNumbers(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  toggleCasMode(currentState: CalculatorSessionState): CalculatorSessionState;
+    toggleCasMode(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  saveVariable(
-    currentState: CalculatorSessionState,
-    variableName: string
-  ): CalculatorSessionState;
+    saveVariable(
+        currentState: CalculatorSessionState,
+        variableName: string,
+    ): CalculatorSessionState;
 
-  deleteVariable(
-    currentState: CalculatorSessionState,
-    variableName: string
-  ): CalculatorSessionState;
+    deleteVariable(
+        currentState: CalculatorSessionState,
+        variableName: string,
+    ): CalculatorSessionState;
 
-  memoryAdd(currentState: CalculatorSessionState): CalculatorSessionState;
+    memoryAdd(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  memorySubtract(currentState: CalculatorSessionState): CalculatorSessionState;
+    memorySubtract(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  memoryRecall(currentState: CalculatorSessionState): CalculatorSessionState;
+    memoryRecall(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  memoryClear(currentState: CalculatorSessionState): CalculatorSessionState;
+    memoryClear(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  simplifyExpression(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState;
+    simplifyExpression(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  expandExpression(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState;
+    expandExpression(currentState: CalculatorSessionState): CalculatorSessionState;
 
-  differentiateExpression(
-    currentState: CalculatorSessionState,
-    variableName: string
-  ): CalculatorSessionState;
+    differentiateExpression(
+        currentState: CalculatorSessionState,
+        variableName: string,
+    ): CalculatorSessionState;
 }
