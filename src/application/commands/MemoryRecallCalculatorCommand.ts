dad612650e7bf75476c640c22a8ec0b20000d9cf
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:25:57 +0800

    feat: add memory recall calculator command

---
+++ src/application/commands/MemoryRecallCalculatorCommand.ts
@@ -0,0 +1,35 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { ExpressionEditingService } from "../../domain/services/ExpressionEditingService";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class MemoryRecallCalculatorCommand extends AbstractCalculatorCommand {
+  public constructor(
+    private readonly expressionEditingService: ExpressionEditingService
+  ) {
+    super();
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    if (currentState.memoryValueText === null) {
+      return currentState;
+    }
+
+    const edit = this.expressionEditingService.insertText(
+      currentState.expressionText,
+      currentState.memoryValueText,
+      currentState.selectionStart,
+      currentState.selectionEnd
+    );
+
+    return currentState.copyWith({
+      expressionText: edit.text,
+      cursorPosition: edit.cursorPosition,
+      selectionStart: edit.selectionStart,
+      selectionEnd: edit.selectionEnd,
+      resultText: null,
+      errorText: null,
+    });
+  }
+}
