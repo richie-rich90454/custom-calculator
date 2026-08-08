a760fa5411f7786a4ecbfc4fede410492a8873c6
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:03:26 +0800

    feat: add numeric mode policy service contract

---
+++ src/domain/services/NumericModePolicyService.ts
@@ -0,0 +1,7 @@
+import { NumericMode } from "../model/NumericMode";
+
+export interface NumericModePolicyService {
+  getSupportedNumericModes(): readonly NumericMode[];
+  isNumericModeSupported(numericMode: NumericMode): boolean;
+  resolveEffectiveNumericMode(requestedNumericMode: NumericMode): NumericMode;
+}
