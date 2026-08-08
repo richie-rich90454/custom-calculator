9bbadb7085566f167992e98da3cefe34ddf4e9d1
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:50 +0800

    style: format src/domain/services/DefaultAngleConversionService.ts with oxfmt

--- src/domain/services/DefaultAngleConversionService.ts
+++ src/domain/services/DefaultAngleConversionService.ts
@@ -1,48 +1,34 @@
 import { AngleMode } from "../model/AngleMode";
 import type { AngleConversionService } from "./AngleConversionService";
 
-export abstract class AbstractAngleConversionService
-  implements AngleConversionService
-{
-  public abstract convertToRadians(
-    value: number,
-    angleMode: AngleMode
-  ): number;
+export abstract class AbstractAngleConversionService implements AngleConversionService {
+    public abstract convertToRadians(value: number, angleMode: AngleMode): number;
 
-  public abstract convertFromRadians(
-    value: number,
-    angleMode: AngleMode
-  ): number;
+    public abstract convertFromRadians(value: number, angleMode: AngleMode): number;
 }
 
 export class DefaultAngleConversionService extends AbstractAngleConversionService {
-  public override convertToRadians(
-    value: number,
-    angleMode: AngleMode
-  ): number {
-    if (angleMode === AngleMode.DEG) {
-      return (value * Math.PI) / 180;
+    public override convertToRadians(value: number, angleMode: AngleMode): number {
+        if (angleMode === AngleMode.DEG) {
+            return (value * Math.PI) / 180;
+        }
+
+        if (angleMode === AngleMode.GON) {
+            return (value * Math.PI) / 200;
+        }
+
+        return value;
     }
 
-    if (angleMode === AngleMode.GON) {
-      return (value * Math.PI) / 200;
+    public override convertFromRadians(value: number, angleMode: AngleMode): number {
+        if (angleMode === AngleMode.DEG) {
+            return (value * 180) / Math.PI;
+        }
+
+        if (angleMode === AngleMode.GON) {
+            return (value * 200) / Math.PI;
+        }
+
+        return value;
     }
-
-    return value;
-  }
-
-  public override convertFromRadians(
-    value: number,
-    angleMode: AngleMode
-  ): number {
-    if (angleMode === AngleMode.DEG) {
-      return (value * 180) / Math.PI;
-    }
-
-    if (angleMode === AngleMode.GON) {
-      return (value * 200) / Math.PI;
-    }
-
-    return value;
-  }
 }
