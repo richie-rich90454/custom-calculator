b5d8fe6160725d587fa6bbc8ac2d12bcbea680f5
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:25 +0800

    style: format src/presentation/components/ConstantsPanelComponent.tsx with oxfmt

--- src/presentation/components/ConstantsPanelComponent.tsx
+++ src/presentation/components/ConstantsPanelComponent.tsx
@@ -12,61 +12,53 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/ConstantsPanelComponent.module.css";
 
 export function ConstantsPanelComponent() {
-  const { store } = useCalculatorApplicationContext();
-  const viewModel = useCalculatorViewModel();
-  const [searchText, setSearchText] = useState("");
-  const [selectedCategoryId, setSelectedCategoryId] = useState(
-    ConstantsPanelViewModel.ALL_CATEGORIES_ID
-  );
+    const { store } = useCalculatorApplicationContext();
+    const viewModel = useCalculatorViewModel();
+    const [searchText, setSearchText] = useState("");
+    const [selectedCategoryId, setSelectedCategoryId] = useState(
+        ConstantsPanelViewModel.ALL_CATEGORIES_ID,
+    );
 
-  const panelViewModel = useMemo(
-    () =>
-      new ConstantsPanelViewModel(
-        viewModel.constants,
-        searchText,
-        selectedCategoryId
-      ),
-    [viewModel.constants, searchText, selectedCategoryId]
-  );
+    const panelViewModel = useMemo(
+        () => new ConstantsPanelViewModel(viewModel.constants, searchText, selectedCategoryId),
+        [viewModel.constants, searchText, selectedCategoryId],
+    );
 
-  const filteredConstants = panelViewModel.filteredConstants;
+    const filteredConstants = panelViewModel.filteredConstants;
 
-  return (
-    <PanelComponent title="Constants">
-      <PanelSectionComponent heading="Find a constant">
-        <ConstantsPanelSearchComponent
-          searchText={searchText}
-          onSearchTextChanged={setSearchText}
-        />
-        <ConstantsPanelCategorySelectorComponent
-          options={panelViewModel.categoryOptions}
-          selectedCategoryId={selectedCategoryId}
-          onCategoryChanged={setSelectedCategoryId}
-        />
-      </PanelSectionComponent>
+    return (
+        <PanelComponent title="Constants">
+            <PanelSectionComponent heading="Find a constant">
+                <ConstantsPanelSearchComponent
+                    searchText={searchText}
+                    onSearchTextChanged={setSearchText}
+                />
+                <ConstantsPanelCategorySelectorComponent
+                    options={panelViewModel.categoryOptions}
+                    selectedCategoryId={selectedCategoryId}
+                    onCategoryChanged={setSelectedCategoryId}
+                />
+            </PanelSectionComponent>
 
-      <PanelSectionComponent heading="Available constants">
-        {filteredConstants.length === 0 ? (
-          <ConstantsPanelEmptyStateComponent
-            hasSearchQuery={panelViewModel.hasSearchQuery}
-          />
-        ) : (
-          <ul
-            className={cssClass(styles.constantList)}
-            aria-label="Constants list"
-          >
-            {filteredConstants.map((constant) => (
-              <ConstantsPanelListItemComponent
-                key={constant.id}
-                constant={constant}
-                onInsert={(constantId) =>
-                  store.getState().onConstantPressed(constantId)
-                }
-              />
-            ))}
-          </ul>
-        )}
-      </PanelSectionComponent>
-    </PanelComponent>
-  );
+            <PanelSectionComponent heading="Available constants">
+                {filteredConstants.length === 0 ? (
+                    <ConstantsPanelEmptyStateComponent
+                        hasSearchQuery={panelViewModel.hasSearchQuery}
+                    />
+                ) : (
+                    <ul className={cssClass(styles.constantList)} aria-label="Constants list">
+                        {filteredConstants.map((constant) => (
+                            <ConstantsPanelListItemComponent
+                                key={constant.id}
+                                constant={constant}
+                                onInsert={(constantId) =>
+                                    store.getState().onConstantPressed(constantId)
+                                }
+                            />
+                        ))}
+                    </ul>
+                )}
+            </PanelSectionComponent>
+        </PanelComponent>
+    );
 }
