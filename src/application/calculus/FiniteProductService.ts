bb344406739b476d464827c57cfd936a309db0ce
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:23 +0800

    style: format src/application/calculus/FiniteProductService.ts with oxfmt

--- src/application/calculus/FiniteProductService.ts
+++ src/application/calculus/FiniteProductService.ts
@@ -1,11 +1,11 @@
 import { AngleMode } from "../../domain/model/AngleMode";
 
 export interface FiniteProductService {
-  product(
-    expressionText: string,
-    variableName: string,
-    lowerBound: number,
-    upperBound: number,
-    angleMode: AngleMode
-  ): number;
+    product(
+        expressionText: string,
+        variableName: string,
+        lowerBound: number,
+        upperBound: number,
+        angleMode: AngleMode,
+    ): number;
 }
