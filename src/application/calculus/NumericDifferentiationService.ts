aeea092647870e1c92bdbaf777b3fff2c20ff116
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 17:06:58 +0800

    feat: add numeric differentiation service contract

---
+++ src/application/calculus/NumericDifferentiationService.ts
@@ -0,0 +1,10 @@
+import { AngleMode } from "../../domain/model/AngleMode";
+
+export interface NumericDifferentiationService {
+  estimateDerivative(
+    expressionText: string,
+    variableName: string,
+    point: number,
+    angleMode: AngleMode
+  ): number;
+}
