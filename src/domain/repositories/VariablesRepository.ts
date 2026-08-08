e645663871693e869b921491c18d8a6399ae25dd
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:48 +0800

    style: format src/domain/repositories/VariablesRepository.ts with oxfmt

--- src/domain/repositories/VariablesRepository.ts
+++ src/domain/repositories/VariablesRepository.ts
@@ -1,8 +1,8 @@
 import { VariableAssignment } from "../model/VariableAssignment";
 
 export interface VariablesRepository {
-  loadVariables(): Promise<readonly VariableAssignment[]>;
-  saveVariable(variable: VariableAssignment): Promise<void>;
-  deleteVariable(variableName: string): Promise<void>;
-  clearVariables(): Promise<void>;
+    loadVariables(): Promise<readonly VariableAssignment[]>;
+    saveVariable(variable: VariableAssignment): Promise<void>;
+    deleteVariable(variableName: string): Promise<void>;
+    clearVariables(): Promise<void>;
 }
