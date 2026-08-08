90e1ff527947496b61511744d2f90827eaf614f1
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:30 +0800

    style: format src/presentation/primitives/AccessibleTextFieldComponent.tsx with oxfmt

--- src/presentation/primitives/AccessibleTextFieldComponent.tsx
+++ src/presentation/primitives/AccessibleTextFieldComponent.tsx
@@ -1,34 +1,22 @@
-import {
-  FieldError,
-  Input,
-  Label,
-  TextField,
-  type TextFieldProps,
-} from "react-aria-components";
+import { FieldError, Input, Label, TextField, type TextFieldProps } from "react-aria-components";
 import { cssClass, joinClassNames } from "../utils/classNames";
 import styles from "../styles/AccessibleTextFieldComponent.module.css";
 
-export interface AccessibleTextFieldComponentProperties
-  extends Omit<TextFieldProps, "className"> {
-  readonly label: string;
-  readonly inputClassName?: string;
+export interface AccessibleTextFieldComponentProperties extends Omit<TextFieldProps, "className"> {
+    readonly label: string;
+    readonly inputClassName?: string;
 }
 
-export function AccessibleTextFieldComponent(
-  props: AccessibleTextFieldComponentProperties
-) {
-  const { label, inputClassName, ...textFieldProperties } = props;
+export function AccessibleTextFieldComponent(props: AccessibleTextFieldComponentProperties) {
+    const { label, inputClassName, ...textFieldProperties } = props;
 
-  const mergedInputClassName = joinClassNames(
-    styles.input,
-    inputClassName
-  );
+    const mergedInputClassName = joinClassNames(styles.input, inputClassName);
 
-  return (
-    <TextField {...textFieldProperties} className={cssClass(styles.textField)}>
-      <Label className={cssClass(styles.label)}>{label}</Label>
-      <Input className={mergedInputClassName} />
-      <FieldError className={cssClass(styles.fieldError)} />
-    </TextField>
-  );
+    return (
+        <TextField {...textFieldProperties} className={cssClass(styles.textField)}>
+            <Label className={cssClass(styles.label)}>{label}</Label>
+            <Input className={mergedInputClassName} />
+            <FieldError className={cssClass(styles.fieldError)} />
+        </TextField>
+    );
 }
