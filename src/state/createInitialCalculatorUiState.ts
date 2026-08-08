e00291f0217392a07ff5cc7466d4222754896f27
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:44:32 +0800

    feat: add initial calculator UI state factory

---
+++ src/state/createInitialCalculatorUiState.ts
@@ -0,0 +1,30 @@
+import { CalculatorSettings } from "../domain/model/CalculatorSettings";
+import { CalculatorPanelName, type CalculatorUiState } from "./CalculatorUiState";
+
+export function createInitialCalculatorUiState(
+  settings: CalculatorSettings,
+  bigIntSupported: boolean,
+  statusMessage: string | null
+): CalculatorUiState {
+  return {
+    expressionText: "",
+    cursorPosition: 0,
+    selectionStart: 0,
+    selectionEnd: 0,
+    resultText: null,
+    errorText: null,
+    lastResultText: null,
+    lastResultValue: null,
+    angleMode: settings.angleMode,
+    numericMode: settings.numericMode,
+    complexNumbersEnabled: settings.complexNumbersEnabled,
+    casEnabled: settings.casEnabled,
+    bigIntSupported: bigIntSupported,
+    themePreference: settings.themePreference,
+    historyEntries: [],
+    variables: [],
+    memoryValueText: null,
+    activePanel: CalculatorPanelName.NONE,
+    statusMessage: statusMessage,
+  };
+}
