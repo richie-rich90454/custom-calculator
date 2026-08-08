6af75b64848dbfaee6f2592108d9f3003d4ec30b
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 10:26:01 +0800

    chore: remove unused render import from settings tests

--- src/presentation/components/CalculatorSettingsAndStatusBar.test.tsx
+++ src/presentation/components/CalculatorSettingsAndStatusBar.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { AngleMode } from "../../domain/model/AngleMode";
 import { createCalculatorTestHarness, renderWithCalculatorContext } from "../../test/calculatorTestHarness";
