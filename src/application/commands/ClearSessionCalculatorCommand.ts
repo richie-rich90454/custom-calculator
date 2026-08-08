f3cd45e62b60b07a0051d1993d6567bf637314c0
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:29 +0800

    style: format src/application/commands/ClearSessionCalculatorCommand.ts with oxfmt

--- src/application/commands/ClearSessionCalculatorCommand.ts
+++ src/application/commands/ClearSessionCalculatorCommand.ts
@@ -2,16 +2,14 @@ import { CalculatorSessionState } from "../../domain/model/CalculatorSessionStat
 import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
 
 export class ClearSessionCalculatorCommand extends AbstractCalculatorCommand {
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    return currentState.copyWith({
-      expressionText: "",
-      cursorPosition: 0,
-      selectionStart: 0,
-      selectionEnd: 0,
-      resultText: null,
-      errorText: null,
-    });
-  }
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        return currentState.copyWith({
+            expressionText: "",
+            cursorPosition: 0,
+            selectionStart: 0,
+            selectionEnd: 0,
+            resultText: null,
+            errorText: null,
+        });
+    }
 }
