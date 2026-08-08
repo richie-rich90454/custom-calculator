c8006a8c1d2941fc9eaf1b3deb66663f4265940a
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:19 +0800

    style: format src/presentation/components/CalculatorCalculusControlPadComponent.tsx with oxfmt

--- src/presentation/components/CalculatorCalculusControlPadComponent.tsx
+++ src/presentation/components/CalculatorCalculusControlPadComponent.tsx
@@ -5,11 +5,11 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/CalculatorKeypadComponent.module.css";
 
 interface CalculatorCalculusControlPadComponentProperties {
-  readonly keys: readonly CalculatorKeyDefinition[];
-  readonly onKeyPressed: (
-    key: CalculatorKeyDefinition,
-    activationKind: ButtonActivationKind
-  ) => void;
+    readonly keys: readonly CalculatorKeyDefinition[];
+    readonly onKeyPressed: (
+        key: CalculatorKeyDefinition,
+        activationKind: ButtonActivationKind,
+    ) => void;
 }
 
 /**
@@ -19,16 +19,16 @@ interface CalculatorCalculusControlPadComponentProperties {
  * available, unlike CAS operations which require an explicit setting.
  */
 export function CalculatorCalculusControlPadComponent(
-  props: CalculatorCalculusControlPadComponentProperties
+    props: CalculatorCalculusControlPadComponentProperties,
 ) {
-  return (
-    <CalculatorKeypadGridSectionComponent
-      sectionClassName={cssClass(styles.calculusArea)}
-      gridLabel="Calculus operations"
-      keys={props.keys}
-      columnCount={4}
-      resolveKeyClassName={() => cssClass(styles.calculusKey)}
-      onKeyPressed={props.onKeyPressed}
-    />
-  );
+    return (
+        <CalculatorKeypadGridSectionComponent
+            sectionClassName={cssClass(styles.calculusArea)}
+            gridLabel="Calculus operations"
+            keys={props.keys}
+            columnCount={4}
+            resolveKeyClassName={() => cssClass(styles.calculusKey)}
+            onKeyPressed={props.onKeyPressed}
+        />
+    );
 }
