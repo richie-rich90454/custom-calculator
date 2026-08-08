ac27745b4915c737e7331da40499b36eb94b653e
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:58 +0800

    style: format src/domain/services/DefaultNumericModePolicyService.ts with oxfmt

--- src/domain/services/DefaultNumericModePolicyService.ts
+++ src/domain/services/DefaultNumericModePolicyService.ts
@@ -2,48 +2,40 @@ import { NumericMode } from "../model/NumericMode";
 import type { BigIntSupportDetector } from "../../infrastructure/featuredetection/BigIntSupportDetector";
 import type { NumericModePolicyService } from "./NumericModePolicyService";
 
-export abstract class AbstractNumericModePolicyService
-  implements NumericModePolicyService
-{
-  public abstract getSupportedNumericModes(): readonly NumericMode[];
-  public abstract isNumericModeSupported(numericMode: NumericMode): boolean;
-  public abstract resolveEffectiveNumericMode(
-    requestedNumericMode: NumericMode
-  ): NumericMode;
+export abstract class AbstractNumericModePolicyService implements NumericModePolicyService {
+    public abstract getSupportedNumericModes(): readonly NumericMode[];
+    public abstract isNumericModeSupported(numericMode: NumericMode): boolean;
+    public abstract resolveEffectiveNumericMode(requestedNumericMode: NumericMode): NumericMode;
 }
 
 export class DefaultNumericModePolicyService extends AbstractNumericModePolicyService {
-  public constructor(
-    private readonly bigIntSupportDetector: BigIntSupportDetector
-  ) {
-    super();
-  }
-
-  public getSupportedNumericModes(): readonly NumericMode[] {
-    const supportedModes: NumericMode[] = [
-      NumericMode.STANDARD,
-      NumericMode.EXACT_DECIMAL,
-      NumericMode.FRACTION,
-    ];
-
-    if (this.bigIntSupportDetector.isBigIntSupported()) {
-      supportedModes.push(NumericMode.BIGINT);
+    public constructor(private readonly bigIntSupportDetector: BigIntSupportDetector) {
+        super();
     }
 
-    return supportedModes;
-  }
+    public getSupportedNumericModes(): readonly NumericMode[] {
+        const supportedModes: NumericMode[] = [
+            NumericMode.STANDARD,
+            NumericMode.EXACT_DECIMAL,
+            NumericMode.FRACTION,
+        ];
 
-  public isNumericModeSupported(numericMode: NumericMode): boolean {
-    return this.getSupportedNumericModes().includes(numericMode);
-  }
+        if (this.bigIntSupportDetector.isBigIntSupported()) {
+            supportedModes.push(NumericMode.BIGINT);
+        }
 
-  public resolveEffectiveNumericMode(
-    requestedNumericMode: NumericMode
-  ): NumericMode {
-    if (this.isNumericModeSupported(requestedNumericMode)) {
-      return requestedNumericMode;
+        return supportedModes;
     }
 
-    return NumericMode.EXACT_DECIMAL;
-  }
+    public isNumericModeSupported(numericMode: NumericMode): boolean {
+        return this.getSupportedNumericModes().includes(numericMode);
+    }
+
+    public resolveEffectiveNumericMode(requestedNumericMode: NumericMode): NumericMode {
+        if (this.isNumericModeSupported(requestedNumericMode)) {
+            return requestedNumericMode;
+        }
+
+        return NumericMode.EXACT_DECIMAL;
+    }
 }
