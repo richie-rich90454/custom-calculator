8e5d4e7d9ba394393fcfdcf50fcaf62c0e144c7c
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:40 +0800

    style: format src/application/commands/MemorySubtractCalculatorCommand.ts with oxfmt

--- src/application/commands/MemorySubtractCalculatorCommand.ts
+++ src/application/commands/MemorySubtractCalculatorCommand.ts
@@ -3,33 +3,29 @@ import type { ExpressionEvaluationGateway } from "../../domain/services/Expressi
 import { AbstractMemoryCalculatorCommand } from "./AbstractMemoryCalculatorCommand";
 
 export class MemorySubtractCalculatorCommand extends AbstractMemoryCalculatorCommand {
-  public constructor(
-    expressionEvaluationGateway: ExpressionEvaluationGateway
-  ) {
-    super(expressionEvaluationGateway);
-  }
-
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    if (currentState.resultText === null) {
-      return currentState;
+    public constructor(expressionEvaluationGateway: ExpressionEvaluationGateway) {
+        super(expressionEvaluationGateway);
     }
 
-    const evaluationResult = this.evaluateMemoryExpression(
-      currentState,
-      currentState.memoryValueText,
-      currentState.resultText,
-      "-"
-    );
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        if (currentState.resultText === null) {
+            return currentState;
+        }
 
-    if (evaluationResult === null) {
-      return currentState;
+        const evaluationResult = this.evaluateMemoryExpression(
+            currentState,
+            currentState.memoryValueText,
+            currentState.resultText,
+            "-",
+        );
+
+        if (evaluationResult === null) {
+            return currentState;
+        }
+
+        return currentState.copyWith({
+            memoryValueText: evaluationResult.resultText,
+            errorText: null,
+        });
     }
-
-    return currentState.copyWith({
-      memoryValueText: evaluationResult.resultText,
-      errorText: null,
-    });
-  }
 }
