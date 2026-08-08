9a1bf1cd8959fd540583b29cca1a98678cdaf60a
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:39 +0800

    style: format src/application/commands/InsertVariableCalculatorCommand.ts with oxfmt

--- src/application/commands/InsertVariableCalculatorCommand.ts
+++ src/application/commands/InsertVariableCalculatorCommand.ts
@@ -3,30 +3,28 @@ import type { ExpressionEditingService } from "../../domain/services/ExpressionE
 import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
 
 export class InsertVariableCalculatorCommand extends AbstractCalculatorCommand {
-  public constructor(
-    private readonly expressionEditingService: ExpressionEditingService,
-    private readonly variableName: string
-  ) {
-    super();
-  }
+    public constructor(
+        private readonly expressionEditingService: ExpressionEditingService,
+        private readonly variableName: string,
+    ) {
+        super();
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    const edit = this.expressionEditingService.insertText(
-      currentState.expressionText,
-      this.variableName,
-      currentState.selectionStart,
-      currentState.selectionEnd
-    );
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        const edit = this.expressionEditingService.insertText(
+            currentState.expressionText,
+            this.variableName,
+            currentState.selectionStart,
+            currentState.selectionEnd,
+        );
 
-    return currentState.copyWith({
-      expressionText: edit.text,
-      cursorPosition: edit.cursorPosition,
-      selectionStart: edit.selectionStart,
-      selectionEnd: edit.selectionEnd,
-      resultText: null,
-      errorText: null,
-    });
-  }
+        return currentState.copyWith({
+            expressionText: edit.text,
+            cursorPosition: edit.cursorPosition,
+            selectionStart: edit.selectionStart,
+            selectionEnd: edit.selectionEnd,
+            resultText: null,
+            errorText: null,
+        });
+    }
 }
