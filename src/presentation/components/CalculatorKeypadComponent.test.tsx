ae111800b41e11a6ec9a38cbda66a88c6bdb4595
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 10:25:47 +0800

    chore: remove unused render import from keypad tests

--- src/presentation/components/CalculatorKeypadComponent.test.tsx
+++ src/presentation/components/CalculatorKeypadComponent.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { createCalculatorTestHarness, renderWithCalculatorContext } from "../../test/calculatorTestHarness";
 import { CalculatorKeypadComponent } from "./CalculatorKeypadComponent";
