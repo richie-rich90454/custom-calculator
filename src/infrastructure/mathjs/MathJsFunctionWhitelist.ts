4d782a3c40e6b1fc27648536764464b589ac9767
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:10:21 +0800

    feat: add math.js function whitelist

---
+++ src/infrastructure/mathjs/MathJsFunctionWhitelist.ts
@@ -0,0 +1,15 @@
+export class MathJsFunctionWhitelist {
+  private readonly allowedFunctionNames: ReadonlySet<string>;
+
+  public constructor(allowedFunctionNames: readonly string[]) {
+    this.allowedFunctionNames = new Set(allowedFunctionNames);
+  }
+
+  public isFunctionAllowed(functionName: string): boolean {
+    return this.allowedFunctionNames.has(functionName);
+  }
+
+  public getAllowedFunctionNames(): readonly string[] {
+    return [...this.allowedFunctionNames];
+  }
+}
