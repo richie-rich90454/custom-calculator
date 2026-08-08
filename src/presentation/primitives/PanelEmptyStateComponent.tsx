784eea893d8acc424d06673f63ce6e027ecbad0f
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:31 +0800

    style: format src/presentation/primitives/PanelEmptyStateComponent.tsx with oxfmt

--- src/presentation/primitives/PanelEmptyStateComponent.tsx
+++ src/presentation/primitives/PanelEmptyStateComponent.tsx
@@ -2,18 +2,16 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/PanelPrimitives.module.css";
 
 interface PanelEmptyStateComponentProperties {
-  readonly message: string;
+    readonly message: string;
 }
 
 /**
  * Consistent empty state message shown when a panel has no content.
  */
-export function PanelEmptyStateComponent(
-  props: PanelEmptyStateComponentProperties
-) {
-  return (
-    <div className={cssClass(styles.emptyState)}>
-      <p className={cssClass(styles.emptyMessage)}>{props.message}</p>
-    </div>
-  );
+export function PanelEmptyStateComponent(props: PanelEmptyStateComponentProperties) {
+    return (
+        <div className={cssClass(styles.emptyState)}>
+            <p className={cssClass(styles.emptyMessage)}>{props.message}</p>
+        </div>
+    );
 }
