c8471bb3eb0599b06030eb898da86aea7b4ded17
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:23:25 +0800

    feat: add delete forward calculator command

---
+++ src/application/commands/DeleteForwardCalculatorCommand.ts
@@ -0,0 +1,30 @@
+import { CalculatorSessionState } from "../../domain/model/CalculatorSessionState";
+import type { ExpressionEditingService } from "../../domain/services/ExpressionEditingService";
+import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
+
+export class DeleteForwardCalculatorCommand extends AbstractCalculatorCommand {
+  public constructor(
+    private readonly expressionEditingService: ExpressionEditingService
+  ) {
+    super();
+  }
+
+  public override execute(
+    currentState: CalculatorSessionState
+  ): CalculatorSessionState {
+    const edit = this.expressionEditingService.deleteForward(
+      currentState.expressionText,
+      currentState.selectionStart,
+      currentState.selectionEnd
+    );
+
+    return currentState.copyWith({
+      expressionText: edit.text,
+      cursorPosition: edit.cursorPosition,
+      selectionStart: edit.selectionStart,
+      selectionEnd: edit.selectionEnd,
+      resultText: null,
+      errorText: null,
+    });
+  }
+}
