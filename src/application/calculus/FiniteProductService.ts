d27f159420ac56707dcb377fa21e9fcfd2ad666c
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 17:06:59 +0800

    feat: add finite product service contract

---
+++ src/application/calculus/FiniteProductService.ts
@@ -0,0 +1,11 @@
+import { AngleMode } from "../../domain/model/AngleMode";
+
+export interface FiniteProductService {
+  product(
+    expressionText: string,
+    variableName: string,
+    lowerBound: number,
+    upperBound: number,
+    angleMode: AngleMode
+  ): number;
+}
