c18b75f12436d2108b523be69a1a27073d7d4613
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:19 +0800

    style: format src/application/calculus/CalculusExpressionRouterService.ts with oxfmt

--- src/application/calculus/CalculusExpressionRouterService.ts
+++ src/application/calculus/CalculusExpressionRouterService.ts
@@ -2,15 +2,15 @@ import { AngleMode } from "../../domain/model/AngleMode";
 import type { CalculusBlockDescriptor } from "./CalculusBlockDescriptor";
 
 export interface CalculusRouteResolution {
-  readonly routed: boolean;
-  readonly resultText: string | null;
-  readonly errorText: string | null;
-  readonly warningText: string | null;
+    readonly routed: boolean;
+    readonly resultText: string | null;
+    readonly errorText: string | null;
+    readonly warningText: string | null;
 }
 
 export interface CalculusExpressionRouterService {
-  routeBlock(
-    blockDescriptor: CalculusBlockDescriptor,
-    angleMode: AngleMode
-  ): CalculusRouteResolution;
+    routeBlock(
+        blockDescriptor: CalculusBlockDescriptor,
+        angleMode: AngleMode,
+    ): CalculusRouteResolution;
 }
