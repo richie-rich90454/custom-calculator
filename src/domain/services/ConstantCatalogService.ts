def86de87ed7f494acd199bb67ae0939765bd4e6
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 08:59:05 +0800

    feat: add constant catalog service contract

---
+++ src/domain/services/ConstantCatalogService.ts
@@ -0,0 +1,9 @@
+import { ScientificConstant } from "../model/ScientificConstant";
+
+export interface ConstantCatalogService {
+  getAllConstants(): readonly ScientificConstant[];
+  getConstantById(constantId: string): ScientificConstant | null;
+  getConstantInsertText(constantId: string): string | null;
+  hasIdentifier(identifier: string): boolean;
+  getConstantIdentifiers(): readonly string[];
+}
