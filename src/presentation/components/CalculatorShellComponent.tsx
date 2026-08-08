442c669260bf2b1c44d74177593d80b9db65afbd
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:52:23 +0800

    style: format src/presentation/components/CalculatorShellComponent.tsx with oxfmt

--- src/presentation/components/CalculatorShellComponent.tsx
+++ src/presentation/components/CalculatorShellComponent.tsx
@@ -18,61 +18,57 @@ import { cssClass } from "../utils/classNames";
 import styles from "../styles/CalculatorShellComponent.module.css";
 
 interface PanelDescriptor {
-  readonly title: string;
-  readonly content: ReactNode;
+    readonly title: string;
+    readonly content: ReactNode;
 }
 
 export function CalculatorShellComponent() {
-  useCalculatorTheme();
+    useCalculatorTheme();
 
-  const { store } = useCalculatorApplicationContext();
-  const viewModel = useCalculatorViewModel();
+    const { store } = useCalculatorApplicationContext();
+    const viewModel = useCalculatorViewModel();
 
-  const panelDescriptor = resolvePanelDescriptor(viewModel.activePanel);
+    const panelDescriptor = resolvePanelDescriptor(viewModel.activePanel);
 
-  return (
-    <div className={cssClass(styles.shell)}>
-      <div className={cssClass(styles.calculator)}>
-        <CalculatorStatusBarComponent />
+    return (
+        <div className={cssClass(styles.shell)}>
+            <div className={cssClass(styles.calculator)}>
+                <CalculatorStatusBarComponent />
 
-        <div className={cssClass(styles.main)}>
-          <CalculatorDisplayComponent />
-          <CalculatorKeypadComponent />
+                <div className={cssClass(styles.main)}>
+                    <CalculatorDisplayComponent />
+                    <CalculatorKeypadComponent />
+                </div>
+            </div>
+
+            <AccessibleDialogComponent
+                title={panelDescriptor.title}
+                isOpen={viewModel.activePanel !== CalculatorPanelName.NONE}
+                onClose={() => store.getState().onPanelOpened(viewModel.activePanel)}
+            >
+                {panelDescriptor.content}
+            </AccessibleDialogComponent>
         </div>
-      </div>
-
-      <AccessibleDialogComponent
-        title={panelDescriptor.title}
-        isOpen={viewModel.activePanel !== CalculatorPanelName.NONE}
-        onClose={() =>
-          store.getState().onPanelOpened(viewModel.activePanel)
-        }
-      >
-        {panelDescriptor.content}
-      </AccessibleDialogComponent>
-    </div>
-  );
+    );
 }
 
-function resolvePanelDescriptor(
-  activePanel: CalculatorPanelName
-): PanelDescriptor {
-  switch (activePanel) {
-    case CalculatorPanelName.HISTORY:
-      return { title: "History", content: <HistoryPanelComponent /> };
-    case CalculatorPanelName.CONSTANTS:
-      return { title: "Constants", content: <ConstantsPanelComponent /> };
-    case CalculatorPanelName.VARIABLES:
-      return { title: "Variables", content: <VariablesPanelComponent /> };
-    case CalculatorPanelName.MEMORY:
-      return { title: "Memory", content: <MemoryPanelComponent /> };
-    case CalculatorPanelName.SETTINGS:
-      return { title: "Settings", content: <SettingsPanelComponent /> };
-    case CalculatorPanelName.CAS:
-      return { title: "CAS", content: <CasPanelComponent /> };
-    case CalculatorPanelName.CALCULUS:
-      return { title: "Calculus", content: <CalculusPanelComponent /> };
-    default:
-      return { title: "Panel", content: null };
-  }
+function resolvePanelDescriptor(activePanel: CalculatorPanelName): PanelDescriptor {
+    switch (activePanel) {
+        case CalculatorPanelName.HISTORY:
+            return { title: "History", content: <HistoryPanelComponent /> };
+        case CalculatorPanelName.CONSTANTS:
+            return { title: "Constants", content: <ConstantsPanelComponent /> };
+        case CalculatorPanelName.VARIABLES:
+            return { title: "Variables", content: <VariablesPanelComponent /> };
+        case CalculatorPanelName.MEMORY:
+            return { title: "Memory", content: <MemoryPanelComponent /> };
+        case CalculatorPanelName.SETTINGS:
+            return { title: "Settings", content: <SettingsPanelComponent /> };
+        case CalculatorPanelName.CAS:
+            return { title: "CAS", content: <CasPanelComponent /> };
+        case CalculatorPanelName.CALCULUS:
+            return { title: "Calculus", content: <CalculusPanelComponent /> };
+        default:
+            return { title: "Panel", content: null };
+    }
 }
