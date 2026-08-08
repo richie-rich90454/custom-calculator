79937d80b7cb80f31db9cdbebff743b76baccda8
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:20:31 +0800

    feat: add static scientific constant repository

---
+++ src/infrastructure/constants/StaticScientificConstantRepository.ts
@@ -0,0 +1,17 @@
+import { ScientificConstant } from "../../domain/model/ScientificConstant";
+import type { ScientificConstantRepository } from "../../domain/repositories/ScientificConstantRepository";
+import { ScientificConstantRegistry } from "./constantRegistry";
+
+export class StaticScientificConstantRepository
+  implements ScientificConstantRepository
+{
+  private readonly constants: readonly ScientificConstant[];
+
+  public constructor() {
+    this.constants = ScientificConstantRegistry.createAllConstants();
+  }
+
+  public loadConstants(): readonly ScientificConstant[] {
+    return this.constants;
+  }
+}
