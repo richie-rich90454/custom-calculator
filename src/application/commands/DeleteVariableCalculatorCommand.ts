2b56644ec5e23dd331c99e4ac3402791f5932a84
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:30 +0800

    style: format src/application/commands/DeleteVariableCalculatorCommand.ts with oxfmt

--- src/application/commands/DeleteVariableCalculatorCommand.ts
+++ src/application/commands/DeleteVariableCalculatorCommand.ts
@@ -2,19 +2,17 @@ import { CalculatorSessionState } from "../../domain/model/CalculatorSessionStat
 import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
 
 export class DeleteVariableCalculatorCommand extends AbstractCalculatorCommand {
-  public constructor(private readonly variableName: string) {
-    super();
-  }
+    public constructor(private readonly variableName: string) {
+        super();
+    }
 
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    const updatedVariables = currentState.variables.filter(
-      (variable) => variable.name !== this.variableName
-    );
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        const updatedVariables = currentState.variables.filter(
+            (variable) => variable.name !== this.variableName,
+        );
 
-    return currentState.copyWith({
-      variables: updatedVariables,
-    });
-  }
+        return currentState.copyWith({
+            variables: updatedVariables,
+        });
+    }
 }
