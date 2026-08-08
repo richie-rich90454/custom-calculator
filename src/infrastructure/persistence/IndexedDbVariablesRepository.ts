a3b1c8fdee3fa4d9316b35ec61ff4327e284e288
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:18:27 +0800

    feat: add IndexedDB variables repository

---
+++ src/infrastructure/persistence/IndexedDbVariablesRepository.ts
@@ -0,0 +1,29 @@
+import { VariableAssignment } from "../../domain/model/VariableAssignment";
+import type { VariablesRepository } from "../../domain/repositories/VariablesRepository";
+import { CalculatorDexieDatabase } from "./CalculatorDexieDatabase";
+
+export class IndexedDbVariablesRepository implements VariablesRepository {
+  public constructor(private readonly database: CalculatorDexieDatabase) {}
+
+  public async loadVariables(): Promise<readonly VariableAssignment[]> {
+    const records = await this.database.variables.toArray();
+
+    return records.map((record) =>
+      this.database.fromVariableAssignmentRecord(record)
+    );
+  }
+
+  public async saveVariable(variable: VariableAssignment): Promise<void> {
+    await this.database.variables.put(
+      this.database.toVariableAssignmentRecord(variable)
+    );
+  }
+
+  public async deleteVariable(variableName: string): Promise<void> {
+    await this.database.variables.delete(variableName);
+  }
+
+  public async clearVariables(): Promise<void> {
+    await this.database.variables.clear();
+  }
+}
