7f7ac3f09108903114692434110bb149e6dc3ba5
richie-rich90454 <rjiang880@gmail.com>
2026-08-08 09:06:24 +0800

    feat: add default numeric mode policy service

---
+++ src/domain/services/DefaultNumericModePolicyService.ts
@@ -0,0 +1,49 @@
+import { NumericMode } from "../model/NumericMode";
+import { BigIntSupportDetector } from "../../infrastructure/featuredetection/BigIntSupportDetector";
+import { NumericModePolicyService } from "./NumericModePolicyService";
+
+export abstract class AbstractNumericModePolicyService
+  implements NumericModePolicyService
+{
+  public abstract getSupportedNumericModes(): readonly NumericMode[];
+  public abstract isNumericModeSupported(numericMode: NumericMode): boolean;
+  public abstract resolveEffectiveNumericMode(
+    requestedNumericMode: NumericMode
+  ): NumericMode;
+}
+
+export class DefaultNumericModePolicyService extends AbstractNumericModePolicyService {
+  public constructor(
+    private readonly bigIntSupportDetector: BigIntSupportDetector
+  ) {
+    super();
+  }
+
+  public getSupportedNumericModes(): readonly NumericMode[] {
+    const supportedModes: NumericMode[] = [
+      NumericMode.STANDARD,
+      NumericMode.EXACT_DECIMAL,
+      NumericMode.FRACTION,
+    ];
+
+    if (this.bigIntSupportDetector.isBigIntSupported()) {
+      supportedModes.push(NumericMode.BIGINT);
+    }
+
+    return supportedModes;
+  }
+
+  public isNumericModeSupported(numericMode: NumericMode): boolean {
+    return this.getSupportedNumericModes().includes(numericMode);
+  }
+
+  public resolveEffectiveNumericMode(
+    requestedNumericMode: NumericMode
+  ): NumericMode {
+    if (this.isNumericModeSupported(requestedNumericMode)) {
+      return requestedNumericMode;
+    }
+
+    return NumericMode.EXACT_DECIMAL;
+  }
+}
