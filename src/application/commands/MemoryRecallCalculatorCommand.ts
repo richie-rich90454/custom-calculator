fc12628c1c524a1e0228b528a600d292178adcb2
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 07:51:40 +0800

    style: format src/application/commands/MemoryRecallCalculatorCommand.ts with oxfmt

--- src/application/commands/MemoryRecallCalculatorCommand.ts
+++ src/application/commands/MemoryRecallCalculatorCommand.ts
@@ -3,33 +3,29 @@ import type { ExpressionEditingService } from "../../domain/services/ExpressionE
 import { AbstractCalculatorCommand } from "./AbstractCalculatorCommand";
 
 export class MemoryRecallCalculatorCommand extends AbstractCalculatorCommand {
-  public constructor(
-    private readonly expressionEditingService: ExpressionEditingService
-  ) {
-    super();
-  }
-
-  public override execute(
-    currentState: CalculatorSessionState
-  ): CalculatorSessionState {
-    if (currentState.memoryValueText === null) {
-      return currentState;
+    public constructor(private readonly expressionEditingService: ExpressionEditingService) {
+        super();
     }
 
-    const edit = this.expressionEditingService.insertText(
-      currentState.expressionText,
-      currentState.memoryValueText,
-      currentState.selectionStart,
-      currentState.selectionEnd
-    );
+    public override execute(currentState: CalculatorSessionState): CalculatorSessionState {
+        if (currentState.memoryValueText === null) {
+            return currentState;
+        }
 
-    return currentState.copyWith({
-      expressionText: edit.text,
-      cursorPosition: edit.cursorPosition,
-      selectionStart: edit.selectionStart,
-      selectionEnd: edit.selectionEnd,
-      resultText: null,
-      errorText: null,
-    });
-  }
+        const edit = this.expressionEditingService.insertText(
+            currentState.expressionText,
+            currentState.memoryValueText,
+            currentState.selectionStart,
+            currentState.selectionEnd,
+        );
+
+        return currentState.copyWith({
+            expressionText: edit.text,
+            cursorPosition: edit.cursorPosition,
+            selectionStart: edit.selectionStart,
+            selectionEnd: edit.selectionEnd,
+            resultText: null,
+            errorText: null,
+        });
+    }
 }
