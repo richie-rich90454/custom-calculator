e3439e730258042c4319647f8ef3b722a8b63986
richie-rich90454 <rjiang880@gmail.com>
2026-08-09 21:05:47 +0800

    feat: initialize app mode and modifier state

--- src/state/createInitialCalculatorUiState.ts
+++ src/state/createInitialCalculatorUiState.ts
@@ -1,10 +1,13 @@
 import { CalculatorSettings } from "../domain/model/CalculatorSettings";
+import { ModifierLayer } from "../domain/model/ModifierLayer";
+import { ResultFormatMode } from "../domain/model/ResultFormatMode";
 import { CalculatorPanelName, type CalculatorUiState } from "./CalculatorUiState";
 
 export function createInitialCalculatorUiState(
     settings: CalculatorSettings,
     bigIntSupported: boolean,
     statusMessage: string | null,
+    activeAppMode: string,
 ): CalculatorUiState {
     return {
         expressionText: "",
@@ -26,5 +29,16 @@ export function createInitialCalculatorUiState(
         memoryValueText: null,
         activePanel: CalculatorPanelName.NONE,
         statusMessage: statusMessage,
+        activeModifierLayer: ModifierLayer.NONE,
+        activeAppMode: activeAppMode,
+        pendingVariablePrompts: [],
+        isStoreModeArmed: false,
+        isRecallModeArmed: false,
+        isEngineeringEnabled: false,
+        isFractionResultDisplayed: false,
+        resultFormatMode: ResultFormatMode.STANDARD,
+        resultFormatDigits: 2,
+        hyperbolicMenuInverse: false,
+        historyReplayIndex: null,
     };
 }
