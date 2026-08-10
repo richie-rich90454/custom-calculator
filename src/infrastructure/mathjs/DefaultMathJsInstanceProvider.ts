5d1466e04279d3e7d323eeaf87f5bfe43f54fb31
richie-rich90454 <rjiang880@gmail.com>
2026-08-10 09:03:58 +0800

    feat: register missing unit names in the mathjs instance

--- src/infrastructure/mathjs/DefaultMathJsInstanceProvider.ts
+++ src/infrastructure/mathjs/DefaultMathJsInstanceProvider.ts
@@ -6,9 +6,19 @@ export class DefaultMathJsInstanceProvider implements MathJsInstanceProvider {
 
     public constructor() {
         this.instance = create(all as FactoryFunctionMap, {});
+        this.registerMissingUnits(this.instance);
     }
 
     public getInstance(): MathJsInstance {
         return this.instance;
+    }
+
+    private registerMissingUnits(math: MathJsInstance): void {
+        math.createUnit("ha", { definition: "1 hectare" });
+        math.createUnit("mph", { definition: "1 mile/hour" });
+        math.createUnit("knot", { definition: "1852 m/hour" });
+        math.createUnit("gon", { definition: "1 grad" });
+        math.createUnit("cal", { definition: "4.184 J" });
+        math.createUnit("kcal", { definition: "4184 J" });
     }
 }
